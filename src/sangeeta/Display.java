dd16aea11857e6458a454e5decc592ed7f7e7f76
Sangeeta Bhandarkar <sangita.bhandarkar@yahoo.in>
2021-03-28 10:20:56 +0530

    Sangeeta Bhandarkar_Assignment_1

---
+++ src/sangeeta/Display.java
@@ -0,0 +1,13 @@
+package sangeeta;
+
+public class Display {
+	
+	void displayName(String name) {
+		System.out.println(" User name is "+ name);		
+	}
+	
+	public static void main(String[] args) {
+		Display display = new Display();
+		display.displayName("Krishna");
+	}
+}
