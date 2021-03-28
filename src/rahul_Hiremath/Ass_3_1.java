5eed87de4c1620308fd01c146438fe8269913beb
Rahul Hiremath <hrahul34@yahoo.com>
2021-03-28 13:19:14 +0530

    Committing assignemnt 2, 3 and 4 programs

---
+++ src/rahul_Hiremath/Ass_3_1.java
@@ -0,0 +1,28 @@
+package rahul_hiremath;
+
+class Ass_3_1
+{
+	String username;
+	String password;
+	int bankAccountNumber;
+	void setDetails(String uname, String pass, int bankAccNo){
+		username = uname;
+		password = pass;
+		bankAccountNumber = bankAccNo;
+	}
+	void updatePassword(String passUpdated){
+		password = passUpdated;
+		System.out.println("Password updated to: " + password);
+	}
+	void displayInfo(){
+		System.out.println("Username is: " + username);
+		System.out.println("Password is: " + password);
+		System.out.println("Bank account number is: "  + bankAccountNumber);
+	}
+	public static void main (String[] args){
+		Ass_3_1 bankAccount = new Ass_3_1();
+		bankAccount.setDetails("Rahul Hiremath", "Password@12", 12345);
+		bankAccount.updatePassword("000000");
+		bankAccount.displayInfo();
+	}
+}
