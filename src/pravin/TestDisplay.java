cebb8f0733ec98ccfeb33d56664ba952019dabce
Pravin Gawali <pmgawali@gmail.com>
2021-03-28 10:14:17 +0530

    pravin_Assignment_1

---
+++ src/pravin/TestDisplay.java
@@ -0,0 +1,18 @@
+package pravin;
+
+class TestDisplay{
+	
+ int age=21;
+ String Name="Techno";
+ 
+ void ShowData(){
+	 
+ System.out.print(age);
+ System.out.print(Name);
+ }
+ 
+ public static void main(String[] args){
+	TestDisplay s1 = new TestDisplay();
+	s1.ShowData();
+    }
+}
