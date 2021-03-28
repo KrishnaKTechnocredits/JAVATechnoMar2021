26ca997be982f6d978a435cebe4cb012e4c1a1df
Ravindra Jadhav <ravindrajadhav841@gmail.com>
2021-03-27 23:44:25 -0500

    Ravindra_Assignment_1

---
+++ src/ravindra_J/Display.java
@@ -0,0 +1,12 @@
+package ravindra_J;
+
+public class Display {
+	
+	void displayName(String name) {
+		System.out.println("User name is: "+name);
+	}
+public static void main(String[] args) {
+	Display display = new Display();
+	display.displayName("Techno");
+}
+}
