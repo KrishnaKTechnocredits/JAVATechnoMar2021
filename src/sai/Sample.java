b0a54d028d18f2c69a0a2e08c34ccab09c08a51f
Sai Krishna NK <nksunny007@gmail.com>
2021-03-28 10:14:45 +0530

    Sai_Assignment_1

---
+++ src/sai/Sample.java
@@ -0,0 +1,13 @@
+package sai;
+
+public class Sample {
+
+	
+	void displayName(String name) {
+		System.out.println("User name is " + name);
+		}
+	public static void main(String[] args) {
+		Sample sample = new Sample();
+		sample.displayName("sai krishna");
+	}
+}	
