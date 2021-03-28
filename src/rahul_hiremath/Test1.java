fb6dbf71c51559bce819d60bd1cfee507360dc65
Rahul Hiremath <hrahul34@yahoo.com>
2021-03-28 11:03:20 +0530

    changes

---
+++ src/rahul_hiremath/Test1.java
@@ -0,0 +1,16 @@
+package rahul_hiremath;
+
+public class Test1 {
+	
+	void display(String name)
+	{
+		System.out.println("The entered name is: " + name);
+	}
+
+	public static void main (String[] args)
+	{
+		Test1 test1 = new Test1();
+		test1.display("Rahul");
+	}
+	
+}
