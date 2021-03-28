849b2a8281558b5b461585784b2f881701806298
Milind Khairnar <kmlnd115@gmail.com>
2021-03-28 17:11:43 +0530

    Milind_Assignment_1_6

---
+++ src/milind/Example1.java
@@ -0,0 +1,16 @@
+package milind;
+/*age print
+ * 
+ */
+class Example1{
+	int age=19;
+	void showData(){
+	System.out.println(age);
+	}
+	
+	public static void main(String [] args)
+	{
+		Example1 s1=new Example1();
+		s1.showData();
+	}
+}
