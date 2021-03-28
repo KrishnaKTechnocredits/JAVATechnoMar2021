73b9287da62f03dcc9121119ec423ae6cd013c1b
Astha <astha.naina@gmail.com>
2021-03-28 11:12:22 +0530

    Astha_Assignment_1

--- src/ashtha/Student.java
+++ src/ashtha/Student.java
@@ -1,5 +1,18 @@
 package ashtha;
 
-public class Student {
-
+public class Student{
+	int age=19;
+	String name = "Astha Prakash";
+	char rating = '@';
+	
+	void showData(){
+	System.out.print(age);
+	System.out.print(name);
+	System.out.print(rating);
+}
+	   
+	public static void main (String[] a) {
+		Student s1 = new Student ();
+		s1.showData();
+	}
 }
