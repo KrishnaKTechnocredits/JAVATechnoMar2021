7e4c951f24cce4ebb7669636848e974a20aee888
pallavi raj <pallavimonika1116@gmail.com>
2021-03-28 18:51:24 +0530

    Pallavi_Assignment_1_5

---
+++ src/pallavi/Huaman.java
@@ -0,0 +1,33 @@
+package pallavi;
+
+class Human{
+	String name;
+	int age;
+	boolean speak;
+	
+	void humanName(String humanName){
+		name=humanName;
+	}
+	
+	void canSpeak(boolean flag){
+		speak=flag;
+	}
+	
+	void humanAge(int humanAge){
+		age=humanAge;
+	}
+	
+	void showDetails(){
+	System.out.println("Name is = " +name);
+	System.out.println("Age is = " +age);
+	System.out.println("Can speak ? = "+speak);
+	}
+	
+	public static void main(String args[]){
+	Human human = new Human();
+	human.humanName("Pallavi Raj");
+	human.humanAge(25);
+	human.canSpeak(true);
+	
+	}
+}
