deb631d040ec587c24f0c7bee07c028686ee9648
Aparna Verma <vermaaparna2017@gmail.com>
2021-03-28 12:51:06 +0530

    Aparna_Assignment_1_5

---
+++ src/aparna/Calculator.java
@@ -0,0 +1,39 @@
+//WAP called Calculator, which has 4 methods.
+//add, sub,div,mul
+//2 instance variable ,x=20 and y=5.
+//1 local variable answer and print the answer in every individual method.
+package aparna;
+
+class Calculator {
+	int x = 20;
+	int y = 5;
+
+	void add() {
+		int answer = x + y;
+		System.out.println("Addition of two Variable is:-> " + answer);
+	}
+
+	void sub() {
+		int answer = x - y;
+		System.out.println("Subtraction of two Variable is:-> " + answer);
+	}
+
+	void mul() {
+		int answer = x * y;
+		System.out.println("Multiplication of two Variable is:-> " + answer);
+	}
+
+	void div() {
+		int answer = x / y;
+		System.out.println("Division of two Variable is :-> " + answer);
+	}
+
+	public static void main(String[] args) {
+
+		Calculator calculator = new Calculator();
+		calculator.add();
+		calculator.sub();
+		calculator.mul();
+		calculator.div();
+	}
+}
