d400e8c0cfd4f44383af49c50cbb319d4227e50e
Sangeeta Bhandarkar <sangita.bhandarkar@yahoo.in>
2021-03-28 10:40:57 +0530

    Sangeeta Bhandarkar_Assignment_1

--- src/sangeeta/Display.java
+++ src/sangeeta/Display.java
@@ -3,7 +3,8 @@ package sangeeta;
 public class Display {
 	
 	void displayName(String name) {
-		System.out.println(" User name is "+ name);		
+		System.out.println(" User name is "+ name);
+		System.out.println(" User name is "+ name);	
 	}
 	
 	public static void main(String[] args) {
