a3e7ec9da249207d43c91e6ecd8ab814b288186c
Bhavana Shinde <mahajanbhavana1489@gmail.com>
2021-03-28 10:14:26 +0530

    Bhavana_Assignment_1

---
+++ src/bhavana/TestFirst.java
@@ -0,0 +1,16 @@
+package bhavana;
+
+public class TestFirst {
+
+	
+	
+	void  display(String person_name) {
+		
+		System.out.println("Name of the user is:");
+	}
+	
+	public static void main(String[] args) {
+		TestFirst TF=new TestFirst();
+		TF.display("Bhavana");
+	}
+}
