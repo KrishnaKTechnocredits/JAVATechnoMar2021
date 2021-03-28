1916f883fa3e500367387c8fe74bcd672afb197a
Krishna Kanani <kananikrishna427@gmail.com>
2021-03-28 10:13:13 +0530

    Krishna_Assignment_1

---
+++ src/technocredits/Display.java
@@ -0,0 +1,13 @@
+package technocredits;
+
+public class Display {
+
+	void displayName(String name) {
+		System.out.println("User Name is: " + name);
+	}
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		display.displayName("Krishna");
+	}
+}
