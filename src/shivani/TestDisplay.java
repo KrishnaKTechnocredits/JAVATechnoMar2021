7ab5a26ff9cedeec9c60321a04835cb3dd55f0b5
Shivani Soni <csshivani98@gmail.com>
2021-03-28 13:29:55 +0530

    shivan_brance1_Assignment1

---
+++ src/shivani/TestDisplay.java
@@ -0,0 +1,15 @@
+package shivani;
+
+public class TestDisplay {
+
+	void display(String name) {
+		System.out.println(name);
+
+	}
+
+	public static void main(String[] args) {
+		TestDisplay testDisplay = new TestDisplay();
+		testDisplay.display("shivani");
+
+	}
+}
