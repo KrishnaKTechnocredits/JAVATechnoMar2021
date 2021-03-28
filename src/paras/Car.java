fae385dabf7839ef1fd065bc16397fbf845fd91c
Paras Rokad <er.pcrokad@gmail.com>
2021-03-28 15:37:19 +0530

    Paras_Assignment_1_5

---
+++ src/paras/Car.java
@@ -0,0 +1,45 @@
+//2. Create a class named Car, which contains method like below
+// 	 a. setcompanyName  
+// 	 b. setIsCarAutomatic  
+// 	 c. setCarSpeed  
+//   d. displaySpecification   
+//Hint: One String, one boolean and one double variable needed.
+
+/*
+Assignment 3 - Program 2 - Program Statement : Create a class named Car, which contains method like below
+*/
+
+package paras;
+
+class Car{
+	
+	String companyName;
+	boolean isCarAutomatic;
+	double carSpeed;
+	
+	void setcompanyName(String name){
+		companyName = name;
+	}
+	
+	void setIsCarAutomatic(boolean isAutomatic){
+		isCarAutomatic = isAutomatic;
+	}
+	
+	void setCarSpeed(double speed){
+		carSpeed = speed;
+	}
+	
+	void displaySpecification(){
+		System.out.println("Name of the Car Company is - " + companyName);
+		System.out.println("Is Car Automatic - " + isCarAutomatic);
+		System.out.println("Speed of the Car is - " + carSpeed);
+	}
+	
+	public static void main(String[] args){
+		Car car = new Car();
+		car.setcompanyName("Hyundai");
+		car.setIsCarAutomatic(true);
+		car.setCarSpeed(95.9);
+		car.displaySpecification();
+	}
+}
---
+++ src/paras/Temperature.java
@@ -0,0 +1,21 @@
+//4. Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+//	 Celsius = (fahrenheit - 32) * 5 / 9
+
+/*
+Assignment 2 - Program 4 - Program Statement : Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+*/
+
+package paras;
+
+class Temperature{
+
+	void convert(float fahrenheit){
+		float Celsius = (fahrenheit - 32) * 5 / 9;
+		System.out.println("Converted temperature from Fahrenheit to Celsius degree is " + Celsius);
+	}
+	
+	public static void main(String[] args){
+		Temperature temperature = new Temperature();
+		temperature.convert(90f);
+	}
+}
