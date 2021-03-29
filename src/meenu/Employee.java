5d322b8158157fa48ad264c844b885e08b278d19
MeenuManhas <meenumanhas4@gmail.com>
2021-03-29 22:32:13 +0530

    Practice of String and integer concatenation

---
+++ src/meenu/Employee.java
@@ -0,0 +1,15 @@
+package meenu;
+
+public class Employee {
+	String name = "Meenu";
+	int x = 1;
+	int y = 2;
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		System.out.println(e1.name+(e1.x+e1.y));
+		// TODO Auto-generated method stub
+
+	}
+
+}
