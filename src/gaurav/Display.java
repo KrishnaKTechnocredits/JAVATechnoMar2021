71097792efb3c91fe0c47b7284be929d05145da7
Gaurav Bhagwat <gauravbhagwat91@gmail.com>
2021-03-29 21:41:45 +0530

    Gaurav_Assignment_1

---
+++ src/gaurav/Display.java
@@ -0,0 +1,17 @@
+package gaurav;
+
+public class Display {
+
+	public void dispayName(String name) {
+	
+		System.out.println("User name is :" +name);
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Display displayName =new Display();
+		displayName.dispayName("Gaurav");
+	}
+
+}
