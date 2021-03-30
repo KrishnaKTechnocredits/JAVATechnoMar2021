94c1af5eeb1816d65c3abb8b68466459274bd049
Pravin Gawali <pmgawali@gmail.com>
2021-03-31 00:02:15 +0530

    pravin_Assignment_7

--- src/pravin/Shop.java
+++ src/pravin/Shop.java
@@ -1,3 +1,19 @@
+/*Assignment no.7
+ * 1. Create a system (Shop) which maintains each item's quantity.
+
+a. Initial stocks of Maggie: 50 Maggie packets 
+b. Initial stocks of Dosa : 43 packets 	
+c. Initial stocks of pouches : 39 oil packets 
+d. Initial stocks of panipuri : 43 packets  
+e. Initial stocks of masala’s : 73 packets.
+
+Expectation:
+1) Only one Customer came for shopping, he picked different items with different quantities, print "running out of stock" message if a particular item is sold out otherwise deduct requested quantity from total quantity. Partial selling is allowed.  
+2) Create a method which shows only those item which is "Out of stock".
+3) Create a method which shows only those item which is "Available in stock" & respective quantities.
+ */
+
+
 package pravin;
 
 public class Shop {
