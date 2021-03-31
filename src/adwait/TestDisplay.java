f9864906d29b4595cba01ce7eb8a830ba1e0bec8
Adwait Jojare <adwaitjojare99@gmail.com>
2021-03-31 00:33:11 -0400

    Adwait_Assignment_1

---
+++ src/adwait/TestDisplay.java
@@ -0,0 +1,19 @@
+package adwait;
+
+public class TestDisplay {
+	
+	String name;
+
+	void setName( String strName){
+		name = strName;
+	}
+	void displayName(){
+		System.out.println("User Name is: "+name);
+	}
+	
+	public static void main(String[] args) {
+		TestDisplay testDisplay = new TestDisplay();
+		testDisplay.setName("Adwait");
+		testDisplay.displayName();		
+	}
+}
