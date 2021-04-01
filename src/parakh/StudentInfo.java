cdb99a670638883ed46ff8e4d4e379cc8a7d2f6f
parakh singhai <parakhsinghai67@gmail.com>
2021-04-02 00:04:10 +0530

    Parakh_Assignment_1-5

---
+++ src/parakh/StudentInfo.java
@@ -0,0 +1,30 @@
+package parakh;
+
+public class StudentInfo {
+
+	      String name = "Shikha";
+		  String middlename = "Amit";
+		  String surname= "Patel";
+		  String birthdate = "10th Aug 1998";
+		  String address = "G-809, Heaven Apartment, Baner, Pune";
+		  int rollNumber = 34;
+		  
+		  void studentName(){
+		  System.out.println(name);
+		  System.out.println(middlename);
+		  System.out.println(surname);
+		  }
+		  
+		  void studentOtherDetails(){
+		  System.out.println(birthdate);
+		  System.out.println(address);
+		  System.out.println(rollNumber);
+		  }
+		  
+		  public static void main(String args[]){
+		  StudentInfo studentInfo = new StudentInfo();
+		  studentInfo.studentName();
+		  studentInfo.studentOtherDetails();
+		
+	}
+}
