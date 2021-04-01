d9914acce8d1319d841ba18bb3839ab9d265e990
Krati Chaturvedi <krati12@gmail.com>
2021-04-01 17:15:48 +0530

    kratic_Assignment_6

--- src/krati_Shukla/Number.java
+++ src/krati_Shukla/Number.java
@@ -2,47 +2,48 @@ package krati_Shukla;
 
 public class Number {
 	
-	void displayEven(int no) {
+	void displayEven(int startIndex,int endIndex) {
 		System.out.println("Even numbers are : ");
-		for (int i=no; i<=15; i=i+2) {
-			System.out.println(i);
+		for (int i=startIndex; i<=endIndex; i++) {
+			if (i%2 == 0)
+				System.out.println(i);
 		}
 	}
 		
-	void displayDiv1(int no) {
+	void displayDiv1(int startIndex,int endIndex) {
 		System.out.println("Numbers divisble by 5 are :");
-		for (int i = no; no <= 30; no = no+1) {
-			if (no%5 == 0)
+		for (int i = startIndex; i <= endIndex; i = i+1) {
+			if (i%5 == 0)
 			{
-				System.out.println(+no+ " ");
+				System.out.println(+i+ " ");
 			}
 		}
 	}
 		
-	void displaydiv2(int no) {
-		for (int i = no; no <= 18; no = no+1) {
-			if (no%5 == 0 && no%3 ==0){
-			System.out.println("Number divisble by 5 & 3 are : " +no+ " ");
+	void displaydiv2(int startIndex,int endIndex) {
+		for (int i = startIndex; i <= endIndex; i = i+1) {
+			if (i%5 == 0 && i%3 ==0){
+			System.out.println("Number divisble by 5 & 3 are : " +i+ " ");
 			}
 		}
 	}
 	
-	void displaydiv3(int no) {
-		for (int i = no; no <= 40; no = no+1) {
-			if (no%7 == 0){
-			System.out.println(no+" is divisble by 7");
+	void displaydiv3(int startIndex,int endIndex) {
+		for (int i = startIndex; i <= 40; i = i+1) {
+			if (i%7 == 0){
+			System.out.println(i+" is divisble by 7");
 			}
-			if (no%13 == 0){
-				System.out.println(no+" is divisble by 13");
+			if (i%13 == 0){
+				System.out.println(i+" is divisble by 13");
 			}	
 		}
 	}
 	public static void main(String[] a) {
 		Number number = new Number();
-		number.displayEven(10);
-		number.displayDiv1(10);
-		number.displaydiv2(5);
-		number.displaydiv3(5);
+		number.displayEven(10,15);
+		number.displayDiv1(10,30);
+		number.displaydiv2(5,18);
+		number.displaydiv3(5,40);
 	}
 
 }
