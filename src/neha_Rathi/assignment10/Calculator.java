86df51d6659a98357e6fe3a3f024651a52a6a7ea
nehatawari <nehatawari3768@gmail.com>
2021-04-03 20:05:42 +0530

    NehaR_Assignment10_code_update

--- src/neha_Rathi/assignment10/Calculator.java
+++ src/neha_Rathi/assignment10/Calculator.java
@@ -2,23 +2,26 @@ package neha_Rathi.assignment10;
 
 public class Calculator {
 
-	int add(int num1, int num2) {
+	long add(int num1, int num2) {
 		return (num1 + num2);
 	}
 
-	int sub(int num1, int num2) {
+	long sub(int num1, int num2) {
 		return (num1 - num2);
 	}
 
-	int mul(int num1, int num2) {
+	double mul(double num1, double num2) {
 		return (num1 * num2);
 	}
 
-	int div(int num1, int num2) {
+	double div(double num1, double num2) {
+		if(num2==0) {
+			return 0;
+		}
 		return (num1 / num2);
 	}
 
-	void printTotalAnswer(int resAdd, int resSub, int resMul, int resDiv) {
+	void printTotalAnswer(long resAdd, long resSub, double resMul, double resDiv) {
 		System.out.println("Sum of all operations is: " + resAdd + " + " + resSub + " + " + resMul + " + " + resDiv
 				+ " = " + (resAdd + resSub + resMul + resDiv));
 	}
@@ -27,9 +30,9 @@ public class Calculator {
 		Calculator calculator = new Calculator();
 		calculator.add(2, 8);
 		calculator.sub(8, 6);
-		calculator.mul(2, 9);
-		calculator.div(10, 2);
-		calculator.printTotalAnswer(calculator.add(2, 8), calculator.sub(8, 6), calculator.mul(2, 9),
+		calculator.mul(2.5d, 9);
+		calculator.div(10, 4);
+		calculator.printTotalAnswer(calculator.add(2, 8), calculator.sub(8, 6), calculator.mul(2.5d, 9),
 				calculator.div(10, 2));
 	}
 }
