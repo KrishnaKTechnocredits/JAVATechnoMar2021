19ecd292ae9ad1fbd98086b914d41a197647dc8a
Simmi <simmisingh217@gmail.com>
2021-04-03 22:06:17 +0530

    All_Assigments_Segregated

--- src/simmi/assignment_8/While.java
+++ src/simmi/assignment_8/While.java
@@ -1,4 +1,45 @@
-package simmi;
+/*Assignment 8 : 1st Apr'2021
+WAP of Assignment 6 using While loop
+
+1. On user define range print all even numbers.
+Hint : pass start and end index as a parameter.
+Input : Range -> 10 to 15
+Output : Even numbers are:
+		 10
+		 12
+		 14	
+
+2. On user define range print all numbers which is divisible by 5.
+Hint : pass start and end index as a parameter.
+Input : Range -> 10 to 30
+Output : Divisible by 5, numbers are:
+		 10
+		 15
+		 20
+		 25
+		 30	
+
+3. On user define range print all numbers which is divisible by 5 and divisible by 3.
+Hint : pass start and end index as a parameter.
+Input : Range -> 5 to 18
+Output : Divisible by 5 & 3, numbers are:
+		 15
+
+4. On user define range print all numbers which is divisible by 7 or 13.
+Hint : pass start and end index as a parameter.
+Input : Range -> 5 to 40
+Output : Divisible by 7 or 13, numbers are:
+		 7 is divisible by 7
+		 13 is divisible by 13
+		 14 is divisible by 7
+		 21 is divisible by 7
+		 26 is divisible by 13
+		 28 is divisible by 7
+		 35 is divisible by 7
+		 39 is divisible by 13
+*/
+
+package simmi.assignment_8;
 
 public class While {
 	
