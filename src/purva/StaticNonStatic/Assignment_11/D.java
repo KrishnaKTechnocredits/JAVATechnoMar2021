70b86a001baf130d2213b96430b0600ca8907342
Purva Sharma <sharmapurva99@gmail.com>
2021-04-04 19:53:27 +0530

    Purva_Assignment_11

---
+++ src/purva/StaticNonStatic/Assignment_11/D.java
@@ -0,0 +1,14 @@
+package purva.StaticNonStatic.Assignment_11;
+
+public class D {
+	
+	static void m6() {
+		System.out.println("In static method m6");
+		m7();
+	}
+	
+	static void m7() {
+		System.out.println("In static method m7");
+	}
+
+}
