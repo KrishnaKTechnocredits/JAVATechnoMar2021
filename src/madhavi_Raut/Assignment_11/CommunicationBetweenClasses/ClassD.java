34dc15f0adbf100835dff3668cdbd3cca44fac1c
Madhavi Raut <madhaviraut2912@gmail.com>
2021-04-04 20:39:43 +0530

    MadhaviRaut_Assignment_11

---
+++ src/madhavi_Raut/Assignment_11/CommunicationBetweenClasses/ClassD.java
@@ -0,0 +1,14 @@
+package madhavi_Raut.Assignment_11.CommunicationBetweenClasses;
+
+public class ClassD {
+
+	static void m6() {
+		ClassD.m7();
+		System.out.println("From ClassD's m6() calling ClassD's static m7()");
+	}
+
+	static void m7() {
+
+		System.out.println("ClassD's static m7()");
+	}
+}
