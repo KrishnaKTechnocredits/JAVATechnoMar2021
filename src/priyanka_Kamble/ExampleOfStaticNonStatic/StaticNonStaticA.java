8da6d8255174ec19b3e4e4ab215fc4ca5f9206d7
priyanka-kamble <priyankakamble1249@gmail.com>
2021-04-04 23:20:14 +0530

    commenting the Problem Status

--- src/priyanka_Kamble/ExampleOfStaticNonStatic/StaticNonStaticA.java
+++ src/priyanka_Kamble/ExampleOfStaticNonStatic/StaticNonStaticA.java
@@ -1,5 +1,15 @@
 package priyanka_Kamble.ExampleOfStaticNonStatic;
 
+/*
+ * Assignment 11 : 4th April 2021
+
+Write a code based on the given figure, [create 4 classes and different methods as per given figure]. 
+from the main method, you will call the m1 method of calls A as mentioned in the figure. From m1 method call m2 method of class B and so on. 
+
+You should write one print statement in every method like Inside First method or anything which can help you to understand which methods are being called in sequence.
+ with acPR number please attach screenshot of output.
+
+ */
 public class StaticNonStaticA {
 	void methodM1() {
 		System.out.println("\t Method M1 of Class A ");
