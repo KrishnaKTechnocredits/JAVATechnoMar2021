1c1d83e5b05242a4996f109695b7c76dde5fa6bb
Gaurav Bhagwat <gauravbhagwat91@gmail.com>
2021-04-05 00:45:24 +0530

    Gaurav_Assignment_11

---
+++ src/gaurav/D.java
@@ -0,0 +1,13 @@
+package gaurav;
+
+public class D {
+	public static void m6()
+	{                                  
+		System.out.println("D method m6");
+		D.m7();
+	}
+		public static void m7()
+		{
+			System.out.println("D method m7");
+		}
+}
