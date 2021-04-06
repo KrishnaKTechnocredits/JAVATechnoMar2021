83b89452121b03b1ac42ab62e53b221830aa81d2
Kangan Kangan <kangan.kangan@singtel.com>
2021-04-06 23:55:17 +0800

    Assignment3_and_12

---
+++ src/kangan/assignments/assignment3/BankAccount.java
@@ -0,0 +1,31 @@
+package kangan.assignments.assignment3;
+
+public class BankAccount {
+
+	 String username;
+	 String password;
+	 int bankAccountNumber;
+	 
+	void setDetails(String uname, String oldpassword, int AccountNumber) {
+		username = uname;
+		password = oldpassword;
+		bankAccountNumber = AccountNumber;
+	}
+	
+	void updatePassword(String newpassword) {
+		password = newpassword;
+	}
+	
+	void displayInfo() {
+	    System.out.println("Username is " + username);
+	    System.out.println("Password is " + password);
+	    System.out.println("bank account number is " + bankAccountNumber);
+	}
+	
+	public static void main(String[] args) {
+		BankAccount bankAccount = new BankAccount();
+		bankAccount.setDetails("Kangan", "kangan234", 1234);
+		bankAccount.updatePassword("kangan678");
+		bankAccount.displayInfo();
+	}
+}
