2ff7eb6e8017ab798d28a92a7466a858bc7df7c8
Madhavi Raut <madhaviraut2912@gmail.com>
2021-04-07 16:48:36 +0530

    MadhaviRaut_Assignment_11_accommodated review comment

--- src/madhavi_Raut/Assignment_11/CommunicationBetweenClasses/ClassD.java
+++ src/madhavi_Raut/Assignment_11/CommunicationBetweenClasses/ClassD.java
@@ -4,11 +4,11 @@ public class ClassD {
 
 	static void m6() {
 		ClassD.m7();
-		System.out.println("From ClassD's static m6() calling ClassD's static m7()");
+		System.out.println("From ClassD's static m6() calling ClassD's static m7().");
 	}
 
 	static void m7() {
 
-		System.out.println("ClassD's static m7()");
+		System.out.println("ClassD's static m7().");
 	}
 }
