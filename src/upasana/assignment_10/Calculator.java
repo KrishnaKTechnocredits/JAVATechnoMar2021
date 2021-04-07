a1416786976f736904d3b2bb4d0071810609ea96
Upasana Anand <upasanaanand06@gmail.com>
2021-04-07 17:37:45 +0530

    Upasana_Assignment_10

---
+++ src/upasana/assignment_10/Calculator.java
@@ -0,0 +1,33 @@
+package upasana.assignment_10;
+
+public class Calculator {
+	
+	int add(int x,int y) {
+		return (x+y);
+	}
+	
+	int sub(int x,int y) {
+		return (x-y);
+	}
+	
+	int div(int x,int y) {
+		return (x/y);
+	}
+	
+	int mul(int x,int y) {
+		return (x*y);
+	}
+	
+	void printTotalAnswer() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		int sum=calculator.add(10, 5);
+		int sub=calculator.sub(10, 5);
+		int div=calculator.div(10, 5);
+		int mul=calculator.mul(10, 5);
+		//calculator.printTotalAnswer(sum,sub,div,mul);
+	}
+}
