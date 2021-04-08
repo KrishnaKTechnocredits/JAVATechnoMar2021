21ae35fd412553c4733c3646c4334acbc4486f1d
Gaurav Bhagwat <gauravbhagwat91@gmail.com>
2021-04-08 18:18:17 +0530

    Gaurav_Assignment_13

--- src/gaurav/SwitchCase.java
+++ src/gaurav/SwitchCase.java
@@ -1,22 +1,22 @@
 package gaurav;
 
-public class SwitchCase{
-	
-	void m1(char ch){
-	   switch(ch) {
-	     case 'd':
-	     System.out.println("d is not a vowel");
-	     break;
-	     case 'I' :
-		 System.out.println("I is a vowel");
-		 break;  
-	     case 'i' :
-		 System.out.println("i is a vowel");
-		 break;
-	    }
-      }		
+public class SwitchCase {
 
-	    public static void main(String[] args) {
+	void m1(char ch) {
+		switch (ch) {
+		case 'd':
+			System.out.println("d is not a vowel");
+			break;
+		case 'I':
+			System.out.println("I is a vowel");
+			break;
+		case 'i':
+			System.out.println("i is a vowel");
+			break;
+		}
+	}
+
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SwitchCase m2 = new SwitchCase();
 		System.out.print(" sample output 1: ");
@@ -25,6 +25,5 @@ public class SwitchCase{
 		m2.m1('I');
 		System.out.print(" sample output 3: ");
 		m2.m1('i');
-   }
+	}
 }
-
