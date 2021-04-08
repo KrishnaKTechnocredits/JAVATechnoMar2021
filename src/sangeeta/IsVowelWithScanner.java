9b21f0f45080886d613c0a4e2a33d59194af2faa
sangita <sangita.bhandarkar@yahoo.in>
2021-04-08 12:31:42 +0530

    Sangeeta_Assignment-14

--- src/sangeeta/IsVowelWithScanner.java
+++ src/sangeeta/IsVowelWithScanner.java
@@ -14,27 +14,23 @@ public class IsVowelWithScanner {
 			ch = Character.toUpperCase(ch);
 			
 			switch(ch) {
-			case 'a':
+			
 			case 'A':
 				System.out.println(" A is a Vowel");
 				break;
 
-			case 'e':
 			case 'E':
 				System.out.println(" E is a Vowel");
 				break;
-
-			case 'i':
+				
 			case 'I':
 				System.out.println(" I is a Vowel");
 				break;
 
-			case 'o':
 			case 'O':
 				System.out.println(" O is a Vowel");
 				break;
 
-			case 'u':
 			case 'U':
 				System.out.println(" U is a Vowel");
 				break;	
