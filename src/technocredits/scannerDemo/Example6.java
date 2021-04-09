13a911d01bc621d78d3c697dfdd6472bb6b22d39
Maulik Kanani <technocredits.oct2020@gmail.com>
2021-04-09 07:38:50 +0530

    Scanner Demo - TechnoCredits session

---
+++ src/technocredits/scannerDemo/Example6.java
@@ -0,0 +1,17 @@
+package technocredits.scannerDemo;
+
+import java.util.Scanner;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter input : ");
+		String str1 = sc.nextLine();
+		System.out.println("Enter input : ");
+		String str = sc.next();
+		
+		System.out.println("str : " + str);
+		System.out.println("str1 :" + str1) ;
+	}
+}
