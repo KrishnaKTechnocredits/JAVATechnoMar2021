401d12bb7a3bc8ab739a9c5624876d93d5c02506
Monika Kaul <monikakaul05@gmail.com>
2021-04-10 19:00:34 +0530

    Monika_assignmnet11

---
+++ src/monika/Assignment11/D_StaticOnly.java
@@ -0,0 +1,13 @@
+package monika.Assignment11;
+
+public class D_StaticOnly {
+	static void m6() {
+		System.out.println("Calling m6 method of class D_StaticOnly");
+		m7();
+	}
+	
+	static void m7() {
+		System.out.println("Calling m7 method of class D_StaticOnly");
+	}
+
+}
