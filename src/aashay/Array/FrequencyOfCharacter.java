ce374245acf768cd7b8727e4add88ff0e9318fdf
Aashay Pilivkar <aashay13@gmail.com>
2021-04-12 14:13:02 +0530

    Aashay_Assignment_16

---
+++ src/aashay/Array/FrequencyOfCharacter.java
@@ -0,0 +1,33 @@
+package aashay.Array;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class FrequencyOfCharacter {
+	
+	void frequency(String text, char c) {
+		int cnt=0;
+		for(int i=0; i < text.length()-1; i++) {
+			if(text.charAt(i)== c) {
+				cnt++;
+			}
+		}
+		System.out.println("Frequency of char "+c+" is: "+cnt);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FrequencyOfCharacter frequencyOfCharacter = new FrequencyOfCharacter();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the text:");
+		String text = scan.next();
+		System.out.println("Enter the character to find its frequency:");
+		char c =  scan.next().charAt(0);
+		frequencyOfCharacter.frequency(text, c);
+		scan.close();
+		
+
+	}
+
+}
