8fed4c7042839c2d7cfde9eea25983708f34834a
Monika Kaul <monikakaul05@gmail.com>
2021-04-12 23:01:50 +0530

    Monika_assignment14

---
+++ src/monika/Assignment14/CalculatorPractise.java
@@ -0,0 +1,42 @@
+package monika.Assignment14;
+
+import java.util.Scanner;
+
+public class CalculatorPractise {
+	 Scanner sc = new Scanner(System.in);
+	
+	int add(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	int sub(int num1) {
+		System.out.println("Provide value for add method");
+		int answer = add(sc.nextInt(),sc.nextInt()) - num1;
+		return answer; 
+	}
+	
+	int mul(int num1) {
+		System.out.println("Provide value for sub method");
+		int answer = sub(sc.nextInt()) * num1;
+		return answer;
+	}
+	
+	int div(int num1) {
+		//System.out.println("Provide value for div method");
+		//int d = sc.nextInt();
+		System.out.println("Provide value for mul method");
+		//double answer = mul(sc.nextInt())/d;
+		//System.out.println("Total calculation : "+answer);
+		int answer = mul(sc.nextInt())/num1;
+		return answer;
+		
+	}
+	
+	public static void main(String[] args) {
+		CalculatorPractise obj = new CalculatorPractise();
+		System.out.println("Calculator program starts from here : ");
+		System.out.println("Total calculation : "+obj.div(5));
+		//obj.div();
+		//obj.div(5);
+	}
+}
