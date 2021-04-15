0340ea1d786786f41c018e73920c5f7e69f76d5c
Mayur Regundwar <mayurregundwar@gmail.com>
2021-04-15 22:13:14 +0530

    Mayur_Assignment_21

---
+++ src/mayur/assignment21/SumChar.java
@@ -0,0 +1,29 @@
+package mayur.assignment21;
+
+import java.util.Scanner;
+
+public class SumChar {
+	int getSum(String str){
+		int charSum = 0;
+		for(int index =0;index<str.length();index++) {
+			if(str.charAt(index)>=48 && str.charAt(index)<=57)
+				charSum = charSum + Character.getNumericValue(str.charAt(index));
+		}
+
+	return charSum;
+
+	}
+
+	public static void main(String[] args) {
+		SumChar sumChar = new SumChar();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the string: " );
+		String str = scanner.next();
+		int sum = sumChar.getSum(str);
+		System.out.println("The sum of entered string is :" +sum);
+		scanner.close();
+	}
+
+
+
+}
