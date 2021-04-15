0e2ce957e89049df1cf91d5e708ca76c2b5f8e46
Dhanshri Singanjude <dhanashri1408@gmail.com>
2021-04-16 03:34:50 +0530

    Dhanshri_Assignment_20

---
+++ src/dhanshri/Assignment_20/StringDigitLowerCaseUpperCaseCount.java
@@ -0,0 +1,39 @@
+package dhanshri.Assignment_20;
+
+public class StringDigitLowerCaseUpperCaseCount {
+	
+	int uppercasecount = 0;
+	int lowerCaseCount = 0;
+	int digit = 0;
+	int specialCharacter;
+
+	void characterCount(String input) {
+		for (int index = 0; index < input.length(); index++) {
+
+			if (Character.isUpperCase(input.charAt(index)))
+				uppercasecount++;
+			else  if (Character.isLowerCase(input.charAt(index)))
+				lowerCaseCount++;
+			else if (Character.isDigit(input.charAt(index)))
+				digit++;
+			}
+		specialCharacter = input.length()-(uppercasecount +lowerCaseCount+digit);
+	}
+	
+	void display() {
+		System.out.println("UpperCase cpunt : " +uppercasecount);
+		System.out.println("LowerCase count : "+lowerCaseCount);
+		System.out.println("Digit count: " +digit);
+		System.out.println("Letter count is: " + (uppercasecount +lowerCaseCount));
+		System.out.println("Special character count is: " +specialCharacter);
+	}
+
+	
+	
+	public static void main(String[] args) {
+		StringDigitLowerCaseUpperCaseCount stringDigitLowerCaseUpperCaseCount = new StringDigitLowerCaseUpperCaseCount();
+		stringDigitLowerCaseUpperCaseCount.characterCount("1rRpd3F9#K(E");
+		stringDigitLowerCaseUpperCaseCount.display();
+		
+	}
+}
