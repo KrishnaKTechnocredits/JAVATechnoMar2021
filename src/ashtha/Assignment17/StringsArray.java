e0f7670462f22f80526a4cfa748583b8e126c063
Astha <astha.naina@gmail.com>
2021-04-15 20:26:16 +0530

    updated

--- src/ashtha/Assignment17/StringsArray.java
+++ src/ashtha/Assignment17/StringsArray.java
@@ -8,41 +8,40 @@ public class StringsArray {
 
 	void printNames(String[] name) {
 		System.out.println("The names in the array are :");
-		for (int index=0;index<name.length;index++) {
-			System.out.print(name[index]+" ");
+		for (int index = 0; index < name.length; index++) {
+			System.out.print(name[index] + " ");
 		}
 	}
-	
+
 	void maximumLength(String[] name) {
 		String maxName = name[0];
 		int maxIndex = 0;
 		for (int index = 1; index < name.length; index++) {
-			if (name[index].length()>maxName.length()) {
+			if (name[index].length() > maxName.length()) {
 				maxName = name[index];
 				maxIndex = index;
 			}
 		}
 		System.out.println("                              ");
-		System.out.println("The maximum length name is "+maxName+" available at index = "+maxIndex);
+		System.out.println("The maximum length name is " + maxName + " available at index = " + maxIndex);
 	}
-	
+
 	public static void main(String[] args) {
 		StringsArray stringsArray = new StringsArray();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the size of array");
 		int size = scanner.nextInt();
 		String[] inputName = new String[size];
-		if(size<0) {
+		if (size < 0) {
 			System.out.println("Enter a valid value");
-		}
-		else{	
-			for (int index = 0 ; index <size; index++) {
-				System.out.println("Enter value "+(index+1)+" in the Array");
+		} else {
+			for (int index = 0; index < size; index++) {
+				System.out.println("Enter value " + (index + 1) + " in the Array");
 				inputName[index] = scanner.next();
-				}
+			}
 		}
 		stringsArray.printNames(inputName);
-		stringsArray.maximumLength(inputName);	
+		stringsArray.maximumLength(inputName);
 		scanner.close();
 	}
 }
