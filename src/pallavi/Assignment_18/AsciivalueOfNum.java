7b163ea45137ced35cd2f4eba3faf2beb1096c7e
pallavi raj <pallavimonika1116@gmail.com>
2021-04-15 20:58:11 +0530

    Changes done

--- src/pallavi/Assignment_18/AsciivalueOfNum.java
+++ src/pallavi/Assignment_18/AsciivalueOfNum.java
@@ -2,8 +2,8 @@ package pallavi.Assignment_18;
 
 public class AsciivalueOfNum {
 	void number() {
-		for(int index= 97;index<=122;index++) {
-			System.out.println("Ascii value of "+index+ " is " +(char)index);
+		for (int index = 97; index <= 122; index++) {
+			System.out.println("Ascii value of " + index + " is " + (char) index);
 		}
 	}
 
--- src/pallavi/Assignment_18/ReturnChar.java
+++ src/pallavi/Assignment_18/ReturnChar.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class ReturnChar {
 
-	
 	char getAsciiOfChar(int asciiValue) {
-		
-		return (char)asciiValue;
-		
+
+		return (char) asciiValue;
+
 	}
+
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter any value");
-		int num=sc.nextInt();
+		int num = sc.nextInt();
 		sc.close();
 		System.out.println(new ReturnChar().getAsciiOfChar(num));
 	}
