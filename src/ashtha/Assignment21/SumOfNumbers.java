0d1bca6899951ac67a866e61742d9a6c29bfc1d0
Astha <astha.naina@gmail.com>
2021-04-17 18:27:08 +0530

    Astha_Assignment_21

--- src/ashtha/Assignment21/SumOfNumbers.java
+++ src/ashtha/Assignment21/SumOfNumbers.java
@@ -3,10 +3,10 @@ package ashtha.Assignment21;
 import java.util.Scanner;
 
 public class SumOfNumbers {
-	
-	int numberSum (String input) {
-		int sum =0;
-		for (int index = 0; index<input.length();index++) {
+
+	int numberSum(String input) {
+		int sum = 0;
+		for (int index = 0; index < input.length(); index++) {
 			int ch = input.charAt(index);
 			if (ch >= 48 && ch <= 57) {
 				ch = Character.getNumericValue(input.charAt(index));
@@ -15,14 +15,15 @@ public class SumOfNumbers {
 		}
 		return sum;
 	}
-	
+
 	public static void main(String[] args) {
 		SumOfNumbers sumOfNumbers = new SumOfNumbers();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the string ");
 		String inputString = scanner.next();
 		int result = sumOfNumbers.numberSum(inputString);
-		System.out.println("Output : "+result);
+		System.out.println("Output : " + result);
+		scanner.close();
 	}
 
 }
