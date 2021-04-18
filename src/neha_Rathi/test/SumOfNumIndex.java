f7d352256861e068674342220a7bd2e7958aa69b
nehatawari <nehatawari3768@gmail.com>
2021-04-18 12:13:02 +0530

    NehaR_Coding_exam_usedArray

--- src/neha_Rathi/test/SumOfNumIndex.java
+++ src/neha_Rathi/test/SumOfNumIndex.java
@@ -1,24 +1,29 @@
+//program1
 package neha_Rathi.test;
 
 public class SumOfNumIndex {
-	int num1=0;
-	int num2=0;
 	
-	void calculateIndex(int[] nums,int targetNum) {
-		for(int index=0;index<nums.length;index++) {
-			for(int innerIndex=0;innerIndex<nums.length;innerIndex++)
-			if(nums[index]+nums[innerIndex]==targetNum) {
-				num1= index;	
-				num2= innerIndex;
-			}
-		}System.out.println("two indexs are: "+ num2 +" & "+ num1);
-		
+	int[] calculateIndex(int[] nums, int targetNum) {
+		int[] output = new int[2];
+		for (int index = 0; index < nums.length; index++) {
+			for (int innerIndex = 0; innerIndex < nums.length; innerIndex++)
+				if (nums[index] + nums[innerIndex] == targetNum) {
+					int count = 0;
+					output[count] = innerIndex;
+					count++;
+					output[count] = index;
+				}
+		}
+		return output;
 	}
 
 	public static void main(String[] args) {
-		SumOfNumIndex sumOfNumIndex= new SumOfNumIndex();
-		int[] nums= {2,11,15,7,4};
-		int targetNum=9;
-		sumOfNumIndex.calculateIndex(nums,targetNum);
+		SumOfNumIndex sumOfNumIndex = new SumOfNumIndex();
+		int[] nums = { 2, 11, 15, 7, 4 };
+		int targetNum = 9;
+		int[] output = (sumOfNumIndex.calculateIndex(nums, targetNum));
+		for (int index = 0; index < output.length; index++) {
+			System.out.println(output[index]);
+		}
 	}
 }
