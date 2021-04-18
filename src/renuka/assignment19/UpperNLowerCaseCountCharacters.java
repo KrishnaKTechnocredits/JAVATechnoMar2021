ecb567342e9c3329e2ef80d42de54b2892913f2a
unknown <renukasahu1994@gmail.com>
2021-04-18 18:45:25 +0530

    Renuka_Assignment19

--- src/renuka/assignment19/UpperNLowerCaseCountCharacters.java
+++ src/renuka/assignment19/UpperNLowerCaseCountCharacters.java
@@ -21,6 +21,7 @@ public class UpperNLowerCaseCountCharacters {
 		UpperNLowerCaseCountCharacters upperNLowerCaseCountCharacters = new UpperNLowerCaseCountCharacters();
 		// String Value =upperNLowerCaseCountCharacters.upperNLowerCaseCountCharacters("TeCHnoSessionS");
 		// String Value1 =upperNLowerCaseCountCharacters.upperNLowerCaseCountCharacters("TECHNOSessionS");
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please Enter Input Name: ");
         String inputName = scanner.next();
