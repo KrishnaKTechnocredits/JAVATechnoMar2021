fdaa951773fe8bf17d66c7438daaf4b44d83f4d0
jadhavravindra <ravindrajadhav841@gmail.com>
2021-04-18 06:26:28 -0500

    Updating_code_after_exam_time

--- src/ravindra_J/Coding_Exam_1/Program1.java
+++ src/ravindra_J/Coding_Exam_1/Program1.java
@@ -10,29 +10,38 @@ Output: Because nums[0] + nums[3] == 9, we return [0, 3].
 package ravindra_J.Coding_Exam_1;
 
 public class Program1 {
-
-	static int [] getIndices(int [] num, int target) {
-		int firstNum = num[0]; 
-		int sum=0 ;
-		int index;
-		for(index=1;index<num.length;index++) {
-			sum = firstNum + num[index];
-			if(target == firstNum + sum)
-				num[1] = num[index];
-			break;
-		}
-		return num;	
-
-	}
-
-	public static void main(String[] args) {
-
-		int[]arr = {2,11,15,7,4};
-		int[]output = getIndices(arr,9);
+	static int num2[] = new int[2];
+	
+	
+	static int [] getIndices(int[] num, int target) {
 		
-		for(int i=0;i<2;i++){
-			System.out.println(arr[i]);
+		for(int i=0;i<num.length;i++) {
+			int first = num[i];
+			for(int j=i+1;j<num.length;j++){
+				int second = num[j];
+				int sum = first + second;
+				if(target == sum) {
+					/*num2[0]=num[i];
+					num2[1]=num[j];*/
+					num2[0]=i;
+					num2[1]=j;
+					break;
+				}	
+			}	
+		}
+			return num2;	
 		}
 	
-}
+	public static void main(String[] args) {
+
+		int[]arr = {5,15,15,2,4};
+		int[]output = getIndices(arr,7);
+
+		for(int i=0;i<2;i++){
+			System.out.println(output[i]);
+		}
+
 	}
+}
+
+
