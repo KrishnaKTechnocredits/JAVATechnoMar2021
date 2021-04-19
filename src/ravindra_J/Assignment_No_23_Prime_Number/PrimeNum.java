1c32fc374a298b2100c1092534fabce43f610e7a
jadhavravindra <ravindrajadhav841@gmail.com>
2021-04-19 16:07:14 -0500

    RJ_Assignment_No_23_2nd_Update_Commit

--- src/ravindra_J/Assignment_No_23_Prime_Number/PrimeNum.java
+++ src/ravindra_J/Assignment_No_23_Prime_Number/PrimeNum.java
@@ -88,33 +88,12 @@ public class PrimeNum {
 		PrimeNum primenum = new PrimeNum();
 		Scanner scanner = new Scanner(System.in);
 
-/*		//validate given number is prime or not.
-		System.out.println("Enter the 1st Number");
+
+		System.out.println("Enter the Number");
 		int n1 = scanner.nextInt();
-		if(n1>1)
-			primenum.checkPrime(n1);
-		else
-			System.out.println("Please entered number should be greater than 1");
+		
+		primenum.checkPrime(n1);
 
-		//printing all prime numbers between given two numbers.
-		System.out.println("Enter the 2nd Number");
-		int n2 = scanner.nextInt();
-		if(n2>n1) {*/
-			int [] out = primenum.getPrimeBetweenRange(2,100);
-			for(int k=0;k<out.length;k++)
-				System.out.println(out[k]);
-/*		}
-		else
-			System.out.println("Please 2nd number should be greater than 1st");
-
-		//printing count of prime numbers between 2 to 100
-		int primeCount = primenum.getCountPrimeBetweenRange(2,100);
-		System.out.println(primeCount);
-
-		//sum of all the prime numbers between 2 to 100
-		int total= primenum.getSumAllPrimeNumBetweenRange(2,100);
-		System.out.println(total);
-*/			
 		primenum.getAverageOfPrimeNumBetweenRange(2, 100);
 		scanner.close();
 	}
