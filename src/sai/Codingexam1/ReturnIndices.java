a92785bb08c4bd0f69be2d7badf7e0d7b49e189b
Sai Krishna NK <nksunny007@gmail.com>
2021-04-19 14:23:24 +0530

    ModifiedAfterComments

--- src/sai/Codingexam1/ReturnIndices.java
+++ src/sai/Codingexam1/ReturnIndices.java
@@ -10,7 +10,7 @@ public class ReturnIndices {
 
 	void addNumber(int[] arr) {
 		for (int index = 0; index < arr.length; index++) {
-			for (int i = 1; i < arr.length; i++) {
+			for (int i = index+1; i < arr.length; i++) {
 				if (arr[index] + arr[i] == 9) {
 					System.out.println("Because the sum of " + "arr[" + index + "]" + "," + "arr[" + i + "] == 9"
 							+ " hence we are the returning the indices as [" + index + "," + i + "]");
