87cfd31d1f937a341913e7bf7a06dfc7c6e66e99
Shilpa Akarte <sh.belsare@gmail.com>
2021-04-19 23:05:14 +0530

    Shilpa_Assignment_2

---
+++ src/shilpa/Assignment_2/Conversion.java
@@ -0,0 +1,17 @@
+//Write a Java program to convert temperature from Fahrenheit to Celsius degree.
+
+package shilpa.Assignment_2;
+
+class Conversion {
+
+	void convert(int fahrenheit) {
+		int celsius = (fahrenheit - 32) * 5 / 9;
+		System.out.println("Temperature in Celcius is: " + celsius);
+	}
+
+	public static void main(String[] arg) {
+		Conversion c1 = new Conversion();
+		c1.convert(98);
+
+	}
+}
