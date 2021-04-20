bc73fffeb75da0d64da368497b4fa65d69169b48
Pravin Gawali <pmgawali@gmail.com>
2021-04-20 23:24:22 +0530

    Pravin_Assignment_22

--- src/pravin/Test.java
+++ src/pravin/Test.java
@@ -3,6 +3,10 @@ package pravin;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Welcom to the Technocredits.");
+		int a[]= new int [4];
+		a[1]=1;
+		a= new int[2];
+		
+		System.out.println("a[1] is "+ a[1]);
 	}
 }
