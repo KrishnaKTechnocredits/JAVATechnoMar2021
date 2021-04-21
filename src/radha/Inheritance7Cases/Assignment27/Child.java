66e6e6d3e0137cfba54f27ec1853c640dc8522ec
Radha Saoji <saojiradha@gmail.com>
2021-04-21 23:37:20 +0530

    Radha_Assignment27

---
+++ src/radha/Inheritance7Cases/Assignment27/Child.java
@@ -0,0 +1,15 @@
+package radha.Inheritance7Cases.Assignment27;
+
+public class Child extends Parent{
+	int y= 30;
+	int z = 40;
+	
+	void m2() {
+		System.out.println("This is method m2 of class Child.");
+	}
+	
+	void m3() {
+		System.out.println("This is method m3 of class Child.");
+	}
+}
+
---
+++ src/radha/Inheritance7Cases/Assignment27/Parent.java
@@ -0,0 +1,15 @@
+package radha.Inheritance7Cases.Assignment27;
+
+public class Parent {
+	int x = 10;
+	int y=12;
+	
+	void m1() {
+		System.out.println("This is method m1 of class Parent.");
+	}
+	
+	void m2() {
+		System.out.println("This is method m2 of class Parent");
+	}
+
+}
