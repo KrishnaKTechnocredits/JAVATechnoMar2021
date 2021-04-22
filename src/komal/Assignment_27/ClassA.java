caf88f0aca175bd345b388326bb734bee68de988
komal kadam <komaljkadam@gmail.com>
2021-04-22 12:36:14 +0530

    Komal_Assignment_27

---
+++ src/komal/Assignment_27/ClassA.java
@@ -0,0 +1,18 @@
+package komal.Assignment_27;
+/*
+Assignment - 27 : 21st April'2021
+Create class Parent and Child. Create a class called Client and execute all 7 inheritance cases there. 
+Note : Child extends Parent
+*/
+
+public class ClassA {
+	int x = 10;
+	int y = 12;
+
+	void classAM1() {
+		System.out.println("Class A M1 Method Called");
+	}
+	void M2() {
+		System.out.println("Class A M2 Method Called");
+	}
+}
