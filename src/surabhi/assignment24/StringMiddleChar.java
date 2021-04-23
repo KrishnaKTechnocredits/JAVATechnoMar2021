8b6aa8ead5c97c48ccbb06076f3492e9dda24b25
surabhi-9 <rathisurabhi45@gmail.com>
2021-04-23 18:26:24 +0530

    Surabhi_Assignment_24

---
+++ src/surabhi/assignment24/StringMiddleChar.java
@@ -0,0 +1,28 @@
+package surabhi.assignment24;
+
+public class StringMiddleChar {
+	char[] findMiddleChar(String[] str) {
+		char[] arr=new char[str.length];
+		for(int index=0;index<str.length;index++) {
+			if(str[index].length()%2!=0) {
+				arr[index]=str[index].charAt(str[index].length()/2);
+			}else
+				arr[index]=str[index].charAt((str[index].length()/2)-1);
+		}
+		return arr;
+	} 
+	 
+	void display(char[] arr) {
+		 for(int index=0; index<arr.length;index++) { 
+			 System.out.println(arr[index]);
+		 }
+	}
+	public static void main(String[] args) {
+		StringMiddleChar stringMiddleChar=new StringMiddleChar();
+		String[] arr= {"Techno", "Hello", "Credits"};
+		char[] result=new char[arr.length];
+		result=stringMiddleChar.findMiddleChar(arr);
+		stringMiddleChar.display(result);
+	}
+
+}
