390c5e3211a3bfc130dbfd00f5012bcfca4a7a52
Anubhav Baiswar <anubhav.baiswar@gmail.com>
2021-04-25 13:58:46 +0530

    src/anubhav/StringOperations/SortingOfCharactersInAString.java

--- src/anubhav/StringOperations/SortingOfCharactersInAString.java
+++ src/anubhav/StringOperations/SortingOfCharactersInAString.java
@@ -2,28 +2,27 @@ package anubhav.StringOperations;
 
 import java.util.Scanner;
 
+/*Write a method to return String having all numeric numbers first followed by Capital letter followed by Small letter. 
+		String msg = "Te1Ch2noCr4e_%$d8iTs9"
+		output : 12489TCCTehnoredis
+ */
+
 public class SortingOfCharactersInAString {
 
 	String sortingOfElements(String input) {
-		String digitsInString    = ""; int digitCount=0;
-		String uppercaseInString = ""; int upperCount=0;
-		String lowercaseInString = ""; int lowerCount=0;
+		String digitsInString    = ""; 
+		String uppercaseInString = ""; 
+		String lowercaseInString = ""; 
 		
 		for (int index = 0; index < input.length(); index++) {
 			if (Character.isDigit(input.charAt(index))) {
 				digitsInString = digitsInString + input.charAt(index);
-				digitCount++;
 			}else if (Character.isUpperCase(input.charAt(index))) {
 				uppercaseInString = uppercaseInString + input.charAt(index);
-				upperCount++;
 			}else if (Character.isLowerCase(input.charAt(index))){
 				lowercaseInString = lowercaseInString + input.charAt(index);	
-				lowerCount++;
 			}
 		}
-		//System.out.println("digit count : " + digitCount);
-		//System.out.println("upper count : " + upperCount);
-		//System.out.println("lower count : " + lowerCount);
 		String output= (digitsInString+uppercaseInString+lowercaseInString);
 		return output;
 	}
@@ -38,5 +37,6 @@ public class SortingOfCharactersInAString {
 		String temp= sortingOfCharactersInAString.sortingOfElements(input);
 		System.out.println(temp);
 		scanner.close();
+		
 	}
 }
