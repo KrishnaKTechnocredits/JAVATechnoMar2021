edb1563d759c589224ace817b8809d976928bbc4
Monika Kaul <monika.kaul@gmail.com>
2021-04-26 01:29:49 +0530

    Monika_assignment21AndByMistake_Added_Assignment23_changestoo

--- src/monika/Assignment21_Assignment20withoutCharFunctions/LogicForCharAsciiValues.java
+++ src/monika/Assignment21_Assignment20withoutCharFunctions/LogicForCharAsciiValues.java
@@ -7,29 +7,19 @@ Ouput: Sum of all digits in String is: 53*/
 public class LogicForCharAsciiValues {
 
 	void getOutput(String input) {	
-		char c1 = '7';
-		int i = (int)c1;
-		char d = '7';
-		int id = (int)d;
-		System.out.println(c1+d);
-		System.out.println(i+id);
-		
-	//	Character.getNumericValue(d);
-		
 		int sum = 0;
 		for(int index=0;index<input.length();index++) {
-			int c = input.charAt(index);
+			int c = input.charAt(index); //'J', '7','y','u','9'
 			if(c>48 && c<=57) {
-				sum+=(c-48);
+				sum+=(c-48); // 0+(49-48)
 			}
 		}
 		System.out.println(sum);
-		System.out.println((char)58);
 	}
 	
 	public static void main(String[] args) {
 		LogicForCharAsciiValues obj = new LogicForCharAsciiValues();
-		obj.getOutput("A");
+		obj.getOutput("J7yu9y8h1h8j4b7j3jjb6");
 	}
 	
 }
