fc949f2eb4827a27a996103de6f25bb22093b137
unknown <renukasahu1994@gmail.com>
2021-04-27 22:15:34 +0530

    Renuka_Assignment27

---
+++ src/renuka/assignment27/Case4.java
@@ -0,0 +1,18 @@
+package renuka.assignment27;
+
+public class Case4 {
+
+	public static void main(String[] args) {
+		//ClassB b1 = new ClassA(); ---CE we cannot create obj of Parent and assign it to child class reference
+				//Typecasting is required.
+
+				/*
+				 * ClassB b1 = (ClassB)new ClassA(); System.out.println(b1.x);
+				 * System.out.println(b1.y); System.out.println(b1.z); b1.m1(); b1.m2();
+				 * b1.m3();
+				 */
+				//ClassCastException
+
+	}
+
+}
