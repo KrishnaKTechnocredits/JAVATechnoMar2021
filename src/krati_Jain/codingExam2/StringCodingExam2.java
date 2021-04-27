d250dc21a940c9d7a703c520f000dfc95f74af4e
Krati Jain <jain.engg07@gmail.com>
2021-04-27 11:38:44 +0530

    kratiJ_coding_exam_2_80

--- src/krati_Jain/codingExam2/StringCodingExam2.java
+++ src/krati_Jain/codingExam2/StringCodingExam2.java
@@ -5,62 +5,39 @@ Input: String str = "12h14i4w8sdc15"*/
 
 package krati_Jain.codingExam2;
 
-public class StringCodingExam2 {
+public class StringCodingExam2 {public static void main(String[] args) {
 
-	public static void main(String[] args) {
+	StringCodingExam2 extraStringCodingExam2 = new StringCodingExam2();
+	String inputStr = "12h14i4w8sdc15";
+	extraStringCodingExam2.getStringWithNum(inputStr);
 
-		StringCodingExam2 stringCodingExam2 = new StringCodingExam2();
-		String inputStr = "12h14i4w8sdc15";
-		System.out.println("Sum of all the numbers in the provided string is: " +stringCodingExam2.getStringWithNum(inputStr));
+}
 
-	}
-
-	int getStringWithNum(String inputStr) {
-		int index = 0;
-		int innerIndex = 0;
-		int sum = 0;
-		int num = 0;
-
-		for (index = 0; index < inputStr.length(); index++) {// 12h14i4w8sdc15
-			int count = 0;
-			String digitFound = ""; // 3/6
-			char charCheck = inputStr.charAt(index);// 1 /4
-			if (Character.isDigit(charCheck)) {// 1/4
-				for (innerIndex = index + 1; innerIndex < inputStr.length(); innerIndex++) {// 1/2/4/5/7
-
-					char nextchar = inputStr.charAt(innerIndex);// 2/h/4/i/w
-					if (Character.isDigit(inputStr.charAt(innerIndex))) {
-						digitFound = digitFound + charCheck + nextchar;// d = 12/14
-						num = Integer.parseInt(digitFound);
-						//System.out.println("digit found: " + num);// 12/14
-						index = innerIndex; // index = 1, 4
-						count++; // c = 1
-
-					} else {
-						index = innerIndex; // index = 2,5
-						if (count == 0) {
-							digitFound = "";
-							digitFound = digitFound + charCheck;
-							num = Integer.parseInt(digitFound);
-							sum = sum + num;
-							//System.out.println("digit found second: " + num);// 12/14
-							//System.out.println("Final Sum 1: " + sum);
-						} else {
-							digitFound = digitFound + charCheck + nextchar;
-							sum = sum + num;
-							//System.out.println("Final Sum 2: " + sum);
-						}
-						break;
-					}
-				}
-
+void getStringWithNum(String inputStr) {
+	int index = 0;
+	String digit = "";
+	int num = 0;
+	int sum = 0;
+	int count = 0;
+	for (; index < inputStr.length(); index++) { // 12h14i4w8sdc15
+		char charCheck = inputStr.charAt(index);
+		if (Character.isDigit(charCheck)) {//1
+			digit += charCheck; // 1
+			//System.out.println("Digit is : " + digit);
+		} else if (Character.isLetter(charCheck)) {
+			if (!digit.equals("")) {
+				num = Integer.parseInt(digit);
+				sum += num;
+				// System.out.println("Sum is: " + sum);
 			}
-
+			digit = "";
 		}
-
-		sum = sum + num;
-	
-		return sum;
-
 	}
+	if (!digit.equals("")) {
+		num = Integer.parseInt(digit);
+		sum += num;
+		System.out.println("Sum of all the numbers in the String is: " + sum);
+	}
+}
+
 }
