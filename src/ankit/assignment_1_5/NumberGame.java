fd83339047bf98c8df85366b3140b39417aebaea
a-malvi <ankit.malvi@gmail.com>
2021-05-03 10:47:47 +0530

    AnkitM_coding_exam_3[35Mins]

--- src/ankit/assignment_1_5/NumberGame.java
+++ src/ankit/assignment_1_5/NumberGame.java
@@ -21,7 +21,7 @@ sop("a max");
 add appropiate logic for b and c. 
 */
 
-package ankit.assignment_01_5;
+package ankit.assignment_1_5;
 class NumberGame{
 	
 	void findMaxNumber(int x, int y, int z){
