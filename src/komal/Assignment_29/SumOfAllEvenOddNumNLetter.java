2010d069fdb16aa89b15bc9ea0a1a3007012ef2b
komal kadam <komaljkadam@gmail.com>
2021-05-04 19:14:19 +0530

    Komal_Assignment_29

--- src/komal/Assignment_29/SumOfAllEvenOddNumNLetter.java
+++ src/komal/Assignment_29/SumOfAllEvenOddNumNLetter.java
@@ -8,51 +8,46 @@ output : 161FTTERTccde68
 Hint : (45+5+111)FTTERTccde(12+56)*/
 
 public class SumOfAllEvenOddNumNLetter {
-	
-	String getOutput(String input) {
-		int evenSum = 0;
-		int oddSum = 0;
-		String upperCaseStr = "";
-		String lowerCaseStr = "";
+
+	void getSum(String input) {
+		int evensum = 0;
+		int oddsum = 0;
+		String uppercase = "";
+		String lowercase = "";
 		String temp = "";
 
 		for (int index = 0; index < input.length(); index++) {
 			char ch = input.charAt(index);
 			if (Character.isDigit(ch)) {
-				temp += ch;
-				if (temp.length() > 0 && index == (input.length() - 1)) {
-					int num = Integer.parseInt(temp);
-					if (num % 2 == 0) {
-						evenSum += num;
-					} else {
-						oddSum += num;
-					}
-					temp = "";
-				}
+				temp = temp + ch;
 			} else if (Character.isLetter(ch)) {
-				if (temp.length() > 0) {
-					int num = Integer.parseInt(temp);
-					if (num % 2 == 0) {
-						evenSum += num;
-					} else {
-						oddSum += num;
-					}
-					temp = "";
+
+				if (!temp.equals(""))
+					if (Integer.parseInt(temp) % 2 == 0)
+						evensum = evensum + Integer.parseInt(temp);
+					else
+						oddsum = oddsum + Integer.parseInt(temp);
+				temp = "";
+
+				if (Character.isUpperCase(ch)) {
+					uppercase = uppercase + ch;
 				}
-				if (Character.isUpperCase(ch))
-					upperCaseStr = upperCaseStr + ch;
-				else
-					lowerCaseStr = lowerCaseStr + ch;
+				if (Character.isLowerCase(ch)) {
+					lowercase = lowercase + ch;
+				}
 			}
+
 		}
-		return oddSum + upperCaseStr + lowerCaseStr + evenSum;
+		if (Integer.parseInt(temp) % 2 == 0)
+			evensum = evensum + Integer.parseInt(temp);
+		else
+			oddsum = oddsum + Integer.parseInt(temp);
+		System.out.println(oddsum + uppercase + lowercase + evensum);
 	}
-		
-		public static void main(String[] args) {
 
-			SumOfAllEvenOddNumNLetter sumOfAllEvenOddNumNLetter = new SumOfAllEvenOddNumNLetter();
-			sumOfAllEvenOddNumNLetter.getOutput("161FTTERTccde68");
+	public static void main(String[] args) {
+		SumOfAllEvenOddNumNLetter sumOfAllEvenOddNumNLetter = new SumOfAllEvenOddNumNLetter();
+		sumOfAllEvenOddNumNLetter.getSum("%F12TT45ERT5cc56de111");
 
-		}
+	}
 }
-
