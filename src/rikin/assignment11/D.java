3f6e4463b0ef05b6037c05c0038a85af5f8ba4aa
Rikin Hingrajiya <rikinhingrajiya@gmail.com>
2021-05-05 19:30:55 +0530

    Rikin_Assignment_11

---
+++ src/rikin/assignment11/D.java
@@ -0,0 +1,13 @@
+package rikin.assignment11;
+
+public class D {
+
+	static void m6() {
+		System.out.println("m6 is static method, in class D.");
+		D.m7();
+	}
+
+	static void m7() {
+		System.out.println("m7 is static method, in class D.");
+	}
+}
