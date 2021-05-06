517b648bba20864dedbdb2e2cc4a28ef0479eaca
Ashish Vyas <ashish.vyas7@gmail.com>
2021-05-06 15:00:41 +0530

    Ashish_Assignment_25

---
+++ src/ashish_Vyas/Assignment_25/ReturnFirstFivePrime.java
@@ -0,0 +1,39 @@
+package ashish_Vyas.Assignment_25;
+
+public class ReturnFirstFivePrime {
+
+	int[] getFirstFivePrime(int start, int end) {
+		int outputIndex=0;
+		 int[] output = new int[5];
+		for (int index = start; index <= end; index++) {
+			boolean flag = true;
+			for (int num = 2; num <= index / 2; num++) {
+				if (index % num == 0) {
+					flag = false;
+					break;
+				}
+			}
+
+			if (flag == true) {
+				// System.out.println("Prime numbers in given range are : " + index);
+				output[outputIndex]=index;
+				outputIndex++;
+		}
+		if(outputIndex==5)
+			break;
+	}
+		return output;
+		
+	}
+
+
+	public static void main(String[] args) {
+		ReturnFirstFivePrime returnFirstFivePrime = new ReturnFirstFivePrime();
+		int[] numbers = returnFirstFivePrime.getFirstFivePrime(2, 20);
+		//System.out.println(numbers);
+		for (int index = 0; index < numbers.length; index++) {
+			if (numbers[index] != 0)
+				System.out.println(numbers[index]);
+	}
+	}
+}
