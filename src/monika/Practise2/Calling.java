56b697e2a02835c2954d079cdf18d2a0f53f4b76
Monika Kaul <monika.kaul@gmail.com>
2021-05-09 00:42:14 +0530

    Monika_practise_work

--- src/monika/Practise2/Calling.java
+++ src/monika/Practise2/Calling.java
@@ -1,30 +1,26 @@
 package monika.Practise2;
+import monika.Practise1.VisibilityPurposeClass;
 
-public class Calling{
-	
-	Calling(){
+
+public class Calling extends VisibilityPurposeClass {
 		
+	Calling(){
+		System.out.println("default child class constructor");
 	}
-	
-	
-	String getOutput(String[] input) {
+	/*String getOutput(String[] input) {
 	String s ="";
 	String d = "_";
 	for(int i=0;i<input.length;i++){
-		s = input[i];
-		if(i<input.length-1)
-		s+=d;
+		s = s + d + input[i];
 	}
-	return s;
-}
-
+	return s.substring(1);
+}*/
 	public static void main(String[] args) {
-		
 		Calling c = new Calling();
-		String[] input = {"Hello","Hi","Techno"};
-		String s = "";
-		s = c.getOutput(input);
-		System.out.println(s);
+		/*String[] input = {"Hello","Hi","Techno"};
+		String s = c.getOutput(input);
+		System.out.println(s);*/
+/*		Manager manager = new Manager(100); //100*/
 	}
 	
 }
