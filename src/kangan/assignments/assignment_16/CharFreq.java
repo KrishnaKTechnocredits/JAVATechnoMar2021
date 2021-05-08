4bb1530a40597ab931e0f501f1dca21577014443
Kangan Kangan <kangan.kangan@singtel.com>
2021-05-08 14:06:19 +0800

    Assignment_15_16

---
+++ src/kangan/assignments/assignment_16/CharFreq.java
@@ -0,0 +1,29 @@
+package kangan.assignments.assignment_16;
+
+import java.util.Scanner;
+
+public class CharFreq {
+
+	void getCharFreq(String input,char temp) {
+		int count = 0;
+		for(int index=0;index<input.length();index++) {
+		char ch = input.charAt(index);
+		if(ch==temp) 
+			count++;
+	}
+		System.out.println(count);
+	}
+	public static void main(String[] args) {
+		CharFreq charFreq = new CharFreq();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter String:- ");
+		//String input = scanner.next();
+		String str = scanner.nextLine();
+		System.out.println("Enter Character you want to check occurance");
+		String str1 = scanner.nextLine();
+		char ch = str1.charAt(0);
+		charFreq.getCharFreq(str, ch);
+		scanner.close();
+		
+	}
+}
