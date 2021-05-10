73059978c79a0089bdacd9e173ff86efcb5c88bf
Monika Kaul <monika.kaul@gmail.com>
2021-05-10 23:52:14 +0530

    practise_work

--- src/monika/Practise1/FindSecondMaxNum.java
+++ src/monika/Practise1/FindSecondMaxNum.java
@@ -6,26 +6,24 @@ int[] arr = {10,33,43,55,97,11,3};
 ouput : 55 */
 
 public class FindSecondMaxNum {
-	void getSecondMaxNum(int[] input) {
-		int[] arr = new int[input.length];
-		int max = arr[0];
-		for(int index=1;index<arr.length;index++) {
-			if(max < arr[index]) {
-				max = arr[index];
-				SecondMax = arr[index+1];
+	int getSecondMaxNum(int[] input) {
+		for(int i=0;i<input.length;i++) {	
+			int temp = 0;
+			for(int j=i+1;j<=input.length;j++) {
+				if(input[i]<input[j-1]) {
+					temp = input[i];
+					input[i] = input[j-1];
+					input[j-1] = temp;
+				}		
 			}
-			if(max > SecondMax)
-				SecondMax = max;
-			
-		}
-		
-		
+		}return input[1];
 	}
 	
 	public static void main(String[] args) {
 		int[] arr = {10,33,43,55,97,11,3};
 		FindSecondMaxNum obj = new FindSecondMaxNum();
-		obj.getSecondMaxNum(arr);
+		int i = obj.getSecondMaxNum(arr);
+		System.out.println(i);
 	}
 	
 }
