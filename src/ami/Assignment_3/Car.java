119efcc2a298339c5426aeba368946da0021525d
Ami-Lakkad <amilakakd@gmail.com>
2021-05-10 18:17:28 +0530

    Ami_Assignment_3M

--- src/ami/Assignment_3/Car.java
+++ src/ami/Assignment_3/Car.java
@@ -11,19 +11,25 @@ public class Car {
 	String companyName;
 	boolean CarAuto;
 	double speed;
-	
+
 	void setcompanyName(String company1) {
 		companyName = company1;
 	}
-	
+
 	void setIsCarAutomatic(boolean CarAuto1) {
 		 CarAuto = CarAuto1;
 	}
-	
+
 	void setCarSpeed(double speed1) {
 		 speed = speed1;
 	}
-	
+
+	void displaySpecification() {
+		System.out.println("Your Company Name : " + companyName);
+		System.out.println("is their car mode Auto : " + CarAuto);
+		System.out.println("What is the normal speed of a car? : " + speed);
+	}
+
 	public static void main(String[] a) {
 		Car car = new Car();
 		car.setcompanyName("Scoda");
