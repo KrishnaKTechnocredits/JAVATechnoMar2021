d942452360745eafcdb7a100c9ca8a03e4d31d2e
Shilpa Akarte <sh.belsare@gmail.com>
2021-05-12 15:16:53 +0530

    shilpaA_coding_exam_4_30mins

--- src/shilpa/coding_exam_4/palindromeString.java
+++ src/shilpa/coding_exam_4/palindromeString.java
@@ -8,23 +8,12 @@ package shilpa.coding_exam_4;
 public class palindromeString {
 	void getPalindromeFrmString(String str) {
 		String[] input = str.split(" ");
-		boolean flag = true;
 		for (int index = 0; index < input.length; index++) {
-			for (int startind = 0, endind = input[index].length() - 1; startind <= endind; startind++, endind--) {
-				if (input[index].charAt(startind) != input[index].charAt(endind)) {
-					flag = false;
-					break;
-				} else
-					flag = true;
-			}
-			if (flag == true) {
-				System.out.println(input[index] + " ");
-			}
-
+			StringBuffer sb1 = new StringBuffer(input[index]);
+			if (input[index].toString().equals(sb1.reverse().toString()))
+				System.out.println(input[index]);
 		}
-
 	}
-
 	public static void main(String[] args) {
 		palindromeString palindromeString = new palindromeString();
 		palindromeString.getPalindromeFrmString("Hi MAM hello NamaN techno");
