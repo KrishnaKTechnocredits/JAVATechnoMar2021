93bf21447656d07b904149f6a492e63ddbba0638
Anubhav Baiswar <anubhav.baiswar@gmail.com>
2021-05-12 10:00:38 +0530

    src/anubhav/StringOperations/Palindrome.java

--- src/anubhav/StringOperations/Palindrome.java
+++ src/anubhav/StringOperations/Palindrome.java
@@ -2,10 +2,10 @@ package anubhav.StringOperations;
 
 public class Palindrome {
 
-	String toCheckString = "Hi Mam Hello Naman Techno";
-	String[] toCheckStringArray = toCheckString.split(" ");
+	String toCheckString = "Hi Mam Hello Naman Techno"; // Input String
+	String[] toCheckStringArray = toCheckString.split(" "); // Converted to String Array
 
-	void palindromeWord(String toCheck) {
+	void palindromeWord(String toCheck) { // Checking palindrome for 1 word
 
 		String toCheckReverse = "";
 		for (int index = toCheck.length() - 1; index >= 0; index--) {
@@ -15,7 +15,7 @@ public class Palindrome {
 			System.out.print(" " + toCheck);
 	}
 
-	void checkAllWords() {
+	void checkAllWords() { // loop to check each element of string array
 		for (int arrayIndex = 0; arrayIndex <= toCheckStringArray.length - 1; arrayIndex++) {
 			palindromeWord(toCheckStringArray[arrayIndex]);
 		}
