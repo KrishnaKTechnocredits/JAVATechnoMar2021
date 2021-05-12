8d28fc1884951f9b3e2234fc1707f2243bbd2c7f
Radha Saoji <saojiradha@gmail.com>
2021-05-12 09:36:21 +0530

    radha_coding_exam_4_30min

---
+++ src/radha/CodingTest4/CheckPalindrome.java
@@ -0,0 +1,30 @@
+package radha.CodingTest4;
+
+public class CheckPalindrome {
+
+	void checkPalindromeString(String str) {
+			String[] arr = str.split(" ");
+			for (int innerIndex = 0; innerIndex < arr.length; innerIndex++) {
+				if(checkPalindromeWord(arr[innerIndex]))
+					System.out.println(arr[innerIndex]);
+			}
+	}
+
+	private boolean checkPalindromeWord(String word) {
+		boolean flag   = false;
+		StringBuffer sb = new StringBuffer(word);
+		sb.reverse();
+
+		if (word.contentEquals(sb))
+			return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args) {
+		CheckPalindrome palindrome = new CheckPalindrome();
+		String input = "Hi MAM hello NamaN techno";
+		palindrome.checkPalindromeString(input);
+	}
+
+}
