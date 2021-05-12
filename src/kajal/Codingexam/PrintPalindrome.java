487c6b18f3ef8874435f42dfe95046040fe61d02
Kajal Kirdat <kajal.kirdat27@gmail.com>
2021-05-12 21:28:51 +0530

    KajalK_coding_exam_4_35 mins

---
+++ src/kajal/Codingexam/PrintPalindrome.java
@@ -0,0 +1,23 @@
+package kajal.Codingexam;
+
+public class PrintPalindrome {
+
+	public void palindrom(String input[]) {
+		for (int index = 0; index <= input.length; index++) {
+			StringBuffer sb = new StringBuffer(input[index]);
+			sb.reverse();
+
+			if (input[index].equals(sb.toString()))
+				System.out.println("Palindrome");
+			else
+				System.out.println("Not Palindrome");
+		}
+	}
+
+	public static void main(String[] args) {
+		PrintPalindrome print = new PrintPalindrome();
+		String string1 = "Hi MAM hello NamaN techno";
+		String str[] = string1.split(" ");
+		print.palindrom(str);
+	}
+}
