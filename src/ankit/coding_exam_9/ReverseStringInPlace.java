c730b7a7b52b4b5684e6cecdf66d16d45f78cab2
a-malvi <ankit.malvi@gmail.com>
2021-05-18 17:57:05 +0530

    AnkitM_coding_exam_9[9Mins]

---
+++ src/ankit/coding_exam_9/ReverseStringInPlace.java
@@ -0,0 +1,19 @@
+package ankit.coding_exam_9;
+
+public class ReverseStringInPlace {
+
+	public static void main(String[] args) {
+		String input = "Techno";
+		char temp = ' ';
+		for(int i = 0 , j = input.length()-1 ; i<j ; i++, j--) {
+			temp = input.charAt(i);
+			//input.charAt(i) = input.charAt(j);
+		//	input.charAt(j) = temp;
+			//pending need to catch with Maulik
+		
+		}
+		}
+		
+
+	}
+
