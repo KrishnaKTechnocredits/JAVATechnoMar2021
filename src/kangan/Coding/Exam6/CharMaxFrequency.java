a20cd7f72b638ffda5f1987c73a8e9ad3804966e
Kangan Kangan <kangan.kangan@singtel.com>
2021-05-20 00:39:16 +0800

    kanganK_coding_Exam6

--- src/kangan/Coding/Exam6/CharMaxFrequency.java
+++ src/kangan/Coding/Exam6/CharMaxFrequency.java
@@ -9,12 +9,12 @@ public class CharMaxFrequency {
 	char getCharWithMaxFreq(String input) {
 		int maxFreq = 0;
 		char maxFreqChar = input.charAt(0);
-		
-      int length = input.length()-1;
-		while (length>=0) {
+
+		int length = input.length() - 1;
+		while (length >= 0) {
 			char ch = input.charAt(length);
 			int initialLength = input.length();
-			String input1 = input.replace(String.valueOf(ch),"");
+			String input1 = input.replace(String.valueOf(ch), "");
 			int newLength = input1.length();
 			int temp = initialLength - newLength;
 			if (temp > maxFreq) {
@@ -26,9 +26,30 @@ public class CharMaxFrequency {
 		return maxFreqChar;
 	}
 
+	char getCharWithMaxFreq1(String input) {
+		int maxFreq = 0;
+		char maxFreqChar = input.charAt(0);
+
+		int length = input.length() - 1;
+		do {
+			char ch = input.charAt(length);
+			int initialLength = input.length();
+			String input1 = input.replace(String.valueOf(ch), "");
+			int newLength = input1.length();
+			int temp = initialLength - newLength;
+			if (temp > maxFreq) {
+				maxFreq = temp;
+				maxFreqChar = ch;
+			}
+
+			length--;
+		} while (length >= 0);
+		return maxFreqChar;
+	}
+
 	public static void main(String[] args) {
 		CharMaxFrequency charMaxFrequency = new CharMaxFrequency();
-		char output = charMaxFrequency.getCharWithMaxFreq("ngnlobant india isn hiringi");
+		char output = charMaxFrequency.getCharWithMaxFreq1("nngnlobant india isn hiringi");
 		System.out.println(output);
 	}
 }
