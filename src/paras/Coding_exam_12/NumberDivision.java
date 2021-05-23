4fc5d196c866babed5140f6d0ba9851d5d4b99ce
Paras Rokad <er.pcrokad@gmail.com>
2021-05-23 13:15:42 +0530

    parasR_coding_exam_12_35min

---
+++ src/paras/Coding_exam_12/NumberDivision.java
@@ -0,0 +1,36 @@
+/*
+Programming Test - 12 : 22nd May'2021
+
+1) ArrayList<Integer> filerNumber(ArrayList<Integer> arr) [15 mins]
+all the numbers, which is div by 3 or by 5 or by 3 & 5.
+
+input : [10,2,3,6,15,44,32]
+output : 10,3,6,15
+ */
+package paras.Coding_exam_12;
+
+import java.util.ArrayList;
+
+public class NumberDivision {
+
+	ArrayList<Integer> filerNumber(ArrayList<Integer> arr) {
+		ArrayList<Integer> output = new ArrayList<Integer>();
+		for (int index = 0; index < arr.size(); index++) {
+			if (arr.get(index) % 3 == 0 || arr.get(index) % 5 == 0)
+				output.add(arr.get(index));
+		}
+		return output;
+	}
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(10);
+		arr.add(2);
+		arr.add(3);
+		arr.add(6);
+		arr.add(15);
+		arr.add(44);
+		arr.add(32);
+		NumberDivision numberDivision = new NumberDivision();
+		System.out.println(numberDivision.filerNumber(arr));
+	}
+}
