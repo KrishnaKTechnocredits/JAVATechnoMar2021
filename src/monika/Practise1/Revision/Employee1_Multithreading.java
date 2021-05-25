cb0303b669a2fa9c0a5d7037d89f9e4203263735
Monika Kaul <monika.kaul@gmail.com>
2021-05-26 02:36:01 +0530

    Monika_practice_work

--- src/monika/Practise1/Revision/Employee1_Multithreading.java
+++ src/monika/Practise1/Revision/Employee1_Multithreading.java
@@ -8,13 +8,19 @@ public class Employee1_Multithreading extends Thread{
 	}
 	
 	public void m1(int input) {
-		int i = input;
-		System.out.println("Hello");
+	int i = input;
+		System.out.println("Hello_employee1 loop");
 		do{
-			System.out.println("Print once");
-			System.out.println("Printing in while loop : "+i);
+			System.out.println("Print once _employee1 loop");
+			System.out.println("Printing in employee1 loop : "+i);
 			i--;
 		}while(i >0 && i <= input);
-	
+		
+		for(int index=0;index<10;index++) {
+			System.out.println("Print employee1 loop : "+index);
+		}
 	}
+	
+	
+	
 }
--- src/monika/Practise1/Revision/Manager_Multithreading.java
+++ src/monika/Practise1/Revision/Manager_Multithreading.java
@@ -1,18 +1,15 @@
 package monika.Practise1.Revision;
 
-import monika.Practise1.Revision2.Employee1;
-import monika.Practise1.Revision2.Employee2;
-
 public class Manager_Multithreading {
 
 	public static void main(String[] args) {
-		Employee1 e1 = new Employee1();
-		Employee2 e2 = new Employee2();
+		Employee1_Multithreading e1 = new Employee1_Multithreading();
+		Employee2_Multithreading e2 = new Employee2_Multithreading();
 		System.out.println("Hi");
 		e1.start();
 		System.out.println("In Middle ");
 		e2.start();
-		e1.start();
+		//e1.start();
 		System.out.println("End");
 	}
 
