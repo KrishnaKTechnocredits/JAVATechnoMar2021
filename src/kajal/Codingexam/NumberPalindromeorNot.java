38bb2fb603f5f2e2867413124481cfa3193ea8bd
Kajal Kirdat <kajal.kirdat27@gmail.com>
2021-05-27 22:41:50 +0530

    17 mins

--- src/kajal/Codingexam/NumberPalindromeorNot.java
+++ src/kajal/Codingexam/NumberPalindromeorNot.java
@@ -4,25 +4,25 @@ output : number is not palindrome*/
 package kajal.Codingexam;
 
 public class NumberPalindromeorNot {
-	
-		void isPailndrome(int input) {
-			int temp = 0;
-			int reversenum = 0;
-			int originalinput = input;
-			while (input != 0) {
-				temp = input % 10;
-				reversenum = reversenum * 10 + temp;
-				input = input / 10;
-			}
-			if (reversenum == originalinput) {
-				System.out.println(originalinput + " is Palindrome");
-			} else {
-				System.out.println(originalinput + " is not Palindrome");
-			}
-		}
 
-		public static void main(String[] args) {
-			new NumberPalindromeorNot().isPailndrome(12323);
-			new NumberPalindromeorNot().isPailndrome(1111);
+	void isPailndrome(int input) {
+		int temp = 0;
+		int reversenum = 0;
+		int originalinput = input;
+		while (input != 0) {
+			temp = input % 10;
+			reversenum = reversenum * 10 + temp;
+			input = input / 10;
+		}
+		if (reversenum == originalinput) {
+			System.out.println(originalinput + " is Palindrome");
+		} else {
+			System.out.println(originalinput + " is not Palindrome");
 		}
 	}
+
+	public static void main(String[] args) {
+		new NumberPalindromeorNot().isPailndrome(12323);
+		new NumberPalindromeorNot().isPailndrome(1111);
+	}
+}
