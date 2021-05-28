3e938628b0229d1641082c51e51cdb6cbca23eb4
Monika Kaul <monika.kaul@gmail.com>
2021-05-28 11:27:04 +0530

    Monika_practicework

--- src/monika/Practise1/Revision/MainOperating.java
+++ src/monika/Practise1/Revision/MainOperating.java
@@ -3,8 +3,9 @@ package monika.Practise1.Revision;
 public class MainOperating {
 
 	public static void main(String[] args) {
-		//UserAccount obj1 = new UserAccount(account1);
-		//UserAccount obj2 = new UserAccount(account1);
+
+		// UserAccount obj1 = new UserAccount(account1);
+		// UserAccount obj2 = new UserAccount(account1);
 		BankOp account1 = new BankOp();
 		BankOp account2 = account1;
 		System.out.println("Start");
