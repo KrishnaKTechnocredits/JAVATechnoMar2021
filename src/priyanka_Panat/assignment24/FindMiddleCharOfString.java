d7b553c2a021d4d875722a37b134506a6fcca1e3
priyanka-kamble <priyankakamble1249@email.com>
2021-05-28 11:02:49 +0530

    K Priyanka - Asssignment_24

--- src/priyanka_Panat/assignment24/FindMiddleCharOfString.java
+++ src/priyanka_Panat/assignment24/FindMiddleCharOfString.java
@@ -1,49 +1,33 @@
-/*
- Assignment - 24 : 18th April'2021
+package priyanka_Panat.assignment24;
+/*Assignment - 24 : 18th April'2021
 
 Create a method which will return middle character of each String. Print output in main method.
 String[] arr = {"Techno", "Hello", "Credits"};
 output : c l d
 
-Note:  for name having even length, consider length/2-1 index is middle character.
- */
-
-package priyanka_Panat.assignment24;
-
-import java.util.Scanner;
+Note: for name having even length, consider length/2-1 index is middle character.*/
 
 public class FindMiddleCharOfString {
 
-	char[] getMiddleChar(String[] arr) {
-		char[] midChar = new char[arr.length];
-		for (int index = 0; index < arr.length; index++) {
-			if (arr[index].length() % 2 == 0) {
-				midChar[index] = arr[index].charAt(arr[index].length() / 2 - 1);
-			} else {
-				midChar[index] = arr[index].charAt(arr[index].length() / 2);
+	void findMiddleChar(String[] array) {
+		//char ch = ' ';
+		int minIndex =0 ;
+		for (int index = 0; index < array.length; index++) {
+			String innerArrayString = array[index];
 
-			}
+			if(innerArrayString.length()%2 == 0)
+				minIndex = innerArrayString.length()/2 - 1 ;
+			else
+				minIndex = innerArrayString.length()/2 ;
+			
+			char ch = innerArrayString.charAt(minIndex);
+			System.out.println(" Minddle Character of " +innerArrayString + " is - '" +ch+ "'");
 		}
-		return midChar;
 	}
 
 	public static void main(String[] args) {
-		FindMiddleCharOfString findMiddleCharOfString = new FindMiddleCharOfString();
-		int size;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of array");
-		size = sc.nextInt();
-		String[] arr = new String[size];
-		System.out.println("Enter the names in an array");
-		for (int i = 0; i < size; i++) {
-			arr[i] = sc.next();
-		}
-		char[] ch1 = findMiddleCharOfString.getMiddleChar(arr);
-		System.out.println("Middle characters in above strings are : ");
-		for (int index = 0; index < ch1.length; index++) {
-			System.out.print(ch1[index] + " ");
-		}
-
-		sc.close();
+		// new FindMiddleCharOfString()
+		String[] array = { "Techno", "Hello", "Credits" };
+		new FindMiddleCharOfString().findMiddleChar(array);
 	}
 }
