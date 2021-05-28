db1a75630539abeb7fc6638cdf2c996cece772d9
surabhi-9 <rathisurabhi45@gmail.com>
2021-05-28 21:51:48 +0530

    RemovedExtraCondtions

--- src/surabhi/coding_exam15/ArrayListFirstWord.java
+++ src/surabhi/coding_exam15/ArrayListFirstWord.java
@@ -7,24 +7,21 @@ public class ArrayListFirstWord {
 	static String getFirstRepeatingNonRepeatingWords(String str) {
 		String[] arr=str.split(" ");
 		String output="";
+		String temp1="", temp2="";
 		boolean isRepeatedFirst=false, isNonRepeated=false;
 		ArrayList<String> input=new ArrayList<String>(Arrays.asList(arr));
+		
 		for(int index=0;index<input.size();index++) {
 			if(input.indexOf(input.get(index))!=input.lastIndexOf(input.get(index)) && !isNonRepeated) {
-				if(!output.isEmpty())
-					output=output+input.get(index)+" ";
-				else
-					output=output+" "+input.get(index);
+				temp1=input.get(index);
 				isNonRepeated=true;
 			}else if(input.indexOf(input.get(index))==input.lastIndexOf(input.get(index)) &&(!isRepeatedFirst)) {
-				if(!output.isEmpty())
-					output=output+" "+input.get(index);
-				else
-					output=output+input.get(index)+" ";
+				temp2=input.get(index);
 				isRepeatedFirst=true;
 			}
 		}
-		return output.trim();
+		output=temp1+" "+temp2;
+		return output;
 	}
 
 	public static void main(String[] args) {
