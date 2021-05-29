817da91c9696346ed3e83ab4cb7bdafd0fb0e2a4
a-malvi <ankit.malvi@gmail.com>
2021-05-29 18:56:23 +0530

    AnkitM_coding_exam_16

---
+++ src/ankit/coding_exam_16/WordFreqInString.java
@@ -0,0 +1,24 @@
+package ankit.coding_exam_16;
+
+import java.util.HashMap;
+
+public class WordFreqInString {
+
+	static HashMap<String ,Integer> getFrequencyOfEachWord(String str){
+		String[] strArr = str.split(" ");
+		HashMap<String, Integer> strHM = new HashMap<String, Integer>();
+		for(String word: strArr) {
+			if(strHM.containsKey(word)) {
+				strHM.put(word, strHM.get(word)+1);
+			}else {
+				strHM.put(word, 1);
+			}		
+		}
+		return strHM;
+	}
+
+	public static void main(String[] args) {
+		String str = "Hi Hi hello Techno Techno Credit Technocredit Hi hello Techno Automation";
+		System.out.println(getFrequencyOfEachWord(str));
+	}
+}
