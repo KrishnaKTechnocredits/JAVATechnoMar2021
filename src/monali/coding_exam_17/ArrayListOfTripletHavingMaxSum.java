4210a565b20275ccb401826c6883c492effe678a
monali tondare <monalitondare1@gmail.com>
2021-05-30 21:24:43 +0530

    monali/coding_exam_17/_time_20mins_15mins_20mins

---
+++ src/monali/coding_exam_17/ArrayListOfTripletHavingMaxSum.java
@@ -0,0 +1,34 @@
+package monali.coding_exam_17;
+
+import java.util.ArrayList;
+
+public class ArrayListOfTripletHavingMaxSum {
+	
+	static int maxsum = 0;
+
+	ArrayList<ArrayList<Integer>> getArrayListOfTriplet(int[] number) {
+		ArrayList<ArrayList<Integer>> tripleList = new ArrayList<ArrayList<Integer>>();
+		for (int index = 0; index < number.length - 2; index = index + 3) {
+			ArrayList<Integer> numberList = new ArrayList<Integer>();
+			numberList.add(number[index]);
+			numberList.add(number[index + 1]);
+			numberList.add(number[index + 2]);
+			int sum = number[index] + number[index + 1] + number[index + 2];
+			if (sum > maxsum) {
+				tripleList.clear();
+				maxsum = sum;
+				tripleList.add(numberList);
+				sum = 0;
+			}
+		}
+
+		return tripleList;
+	}
+
+	public static void main(String[] args) {
+		ArrayListOfTripletHavingMaxSum triple = new ArrayListOfTripletHavingMaxSum();
+		int[] number = { 1, 10, 2, 3, 1, 11, 4, 2, 1 };
+		ArrayList<ArrayList<Integer>> output = triple.getArrayListOfTriplet(number);
+		System.out.println("Triplet having maximum sum of " + maxsum + " is " + output);
+	}
+}
