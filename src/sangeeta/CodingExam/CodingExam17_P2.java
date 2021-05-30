3f6392f27f250c975148a1900f6aa8f58c994287
sangita <sangita.bhandarkar@yahoo.in>
2021-05-30 20:22:12 +0530

    sangeetaB_CodingExam17_90Min

---
+++ src/sangeeta/CodingExam/CodingExam17_P2.java
@@ -0,0 +1,36 @@
+/*triplet having maximum sum, index overlapping is allowed.
+input : [1,10,2,3,1,11,4,2,1]
+output : 11,4,2 -> 17*/
+
+package sangeeta.CodingExam;
+
+import java.util.ArrayList;
+
+public class CodingExam17_P2 {
+static int maxSum;
+	
+	ArrayList<ArrayList<Integer>> tripletHavingMaxSum(int [] arr){
+		ArrayList<ArrayList<Integer>> triplets = new ArrayList<ArrayList<Integer>>();
+		for(int index = 0; index < arr.length - 3 ; index++) {
+			ArrayList<Integer> numberList = new ArrayList<Integer>();
+			numberList.add(arr[index]);
+			numberList.add(arr[index + 1]);
+			numberList.add(arr[index + 2]);
+			int sum = arr[index] + arr[index+1] + arr[index+2];
+			if(sum > maxSum) {
+					triplets.clear();
+					maxSum = sum;
+					triplets.add(numberList);
+					sum = 0;
+				}
+		}
+		return triplets;
+	}
+	
+	public static void main(String[] args) {
+		CodingExam17_P2 codingExam17_P2 = new CodingExam17_P2();
+		int [] arr = {1,10,2,3,1,11,4,2,1};
+		ArrayList<ArrayList<Integer>> output = codingExam17_P2.tripletHavingMaxSum(arr);
+		System.out.println("Triplet having maximum sum of "+ maxSum +" is "+output);
+	}
+}
