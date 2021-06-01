659a3cb4d8353eb5ac7eee10ed43adf5da3aaaeb
Rupali Lahoti <rupalee28@gmail.com>
2021-06-01 18:01:11 +0530

    RupaliL_coding_exam_17

---
+++ src/rupali/codingexam/exam17/Maxsumoftripletwithoverlapping.java
@@ -0,0 +1,31 @@
+package rupali.codingexam.exam17;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Maxsumoftripletwithoverlapping {
+	ArrayList<Integer> getmaxtriplet(int[] input){
+		ArrayList<Integer> numberlist=new ArrayList<Integer>();
+		int max=0,sum=0;
+		int length=input.length-(input.length%3)-2;
+		
+		for (int index=0;index<length;index++) {
+			max=input[index]+input[index+1]+input[index+2];
+			if(sum<max) {
+			numberlist.clear();
+			sum=max;
+			numberlist.add(input[index]);
+			numberlist.add(input[index+1]);
+			numberlist.add(input[index+2]);
+			}
+		}
+		return numberlist;
+	}
+	
+	public static void main(String[] args) {
+		int[] input= {1,10,2,3,1,11,4,2,1};
+		System.out.println(Arrays.toString(input));
+		System.out.println("output: "+new Maxsumoftripletwithoverlapping().getmaxtriplet(input));
+		
+	}
+}
