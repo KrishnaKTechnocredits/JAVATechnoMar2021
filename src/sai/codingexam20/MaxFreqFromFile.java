860afaf008c115a38685c8f50e83f7f3f9ca225a
Sai Krishna NK <nksunny007@gmail.com>
2021-06-04 01:45:26 +0530

    SaiKrishna_coding_exam20_35mins

---
+++ src/sai/codingexam20/MaxFreqFromFile.java
@@ -0,0 +1,36 @@
+package sai.codingexam20;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MaxFreqFromFile {
+
+	String maxFreqWord(String word) throws FileNotFoundException {
+		Scanner sc = new Scanner(new File(".\\src\\sai\\codingexam20\\Input.txt"));
+		int countMax = 0;
+		String strLine = "";
+		while (sc.hasNextLine()) {
+			int count = 0;
+			String line = sc.nextLine();
+			String originalLine = line;
+			while (line.contains(word)) {
+				count++;
+				line = line.replaceFirst(word, "");
+			}
+			if (count > countMax) {
+				countMax = count;
+				strLine = originalLine;
+			}
+		}
+		sc.close();
+		return strLine;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		MaxFreqFromFile maxFreqFromFile = new MaxFreqFromFile();
+		System.out.println("Line having maximum frequency of Techno: " + maxFreqFromFile.maxFreqWord("techno"));
+	}
+}
