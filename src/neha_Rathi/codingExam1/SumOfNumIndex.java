c120de736551a47f61e486e899dea143d46c44aa
nehatawari <nehatawari3768@gmail.com>
2021-06-03 16:51:32 +0530

    File_Change

--- src/neha_Rathi/codingExam1/SumOfNumIndex.java
+++ src/neha_Rathi/codingExam1/SumOfNumIndex.java
@@ -1,5 +1,11 @@
-//program1
-package neha_Rathi.test;
+//Program 1 :
+//Given an array of integers nums and an integer target, 
+//return indices of the two numbers such that they add up to the target.
+//Input: nums = [2,11,15,7,4], target = 9
+//Output: [0,3]
+//Output: Because nums[0] + nums[3] == 9, we return [0, 3].
+
+package neha_Rathi.codingExam1;
 
 public class SumOfNumIndex {
 	
