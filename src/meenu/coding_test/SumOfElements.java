ac4d29036f58ea3c3d4fa9fd97b420baa5943ea8
MeenuManhas <meenumanhas4@gmail.com>
2021-06-26 13:32:21 +0530

    Meenu_coding_test_25min

--- src/meenu/coding_test/SumOfElements.java
+++ src/meenu/coding_test/SumOfElements.java
@@ -1,6 +1,6 @@
 package meenu.coding_test;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 /*WAP to return the sum of all elements of an array, except ignore sections of numbers 
 starting with 6 and extending to the next 9. Return 0 for No numbers.
@@ -34,10 +34,20 @@ public class SumOfElements {
 	}
 
 	public static void main(String[] args) {		
-		int[] array = {6,1,4,5,9};			
-		int sum = getElementsSum(array);
-		System.out.println(sum);
-
+		int[] array1 = {1,3,5};		
+		int[] array2 = {4,5,6,12,2,9,1,3};
+		int[] array3 = {2,1,6,9,11,6,19,12,9,2};
+		int[] array4 = {6,1,4,5,9};
+		int[][] finalArray = new int[4][4];
+		finalArray[0] = array1;
+		finalArray[1] = array2;
+		finalArray[2] = array3;
+		finalArray[3] = array4;
+		
+		for(int index =0;index<finalArray.length;index++) {
+			System.out.println("Input -> " + Arrays.toString(finalArray[index]));
+			int sum = getElementsSum(finalArray[index]);
+			System.out.println("Output -> " +sum);
+		}	
 	}
-
 }
